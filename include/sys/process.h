48adaafbc961244c5647c0876e17d0248a716ed8
ireader <iguest@sogou.com>
2014-10-18 15:41:52 +0800

    use pthread_t as thread handle to avoid MAC thread_t type

--- include/sys/process.h
+++ include/sys/process.h
@@ -20,7 +20,7 @@ typedef struct
 {
 	DWORD id;
 	HANDLE handle;
-} thread_t;
+} pthread_t;
 
 typedef DWORD			process_t;
 
@@ -34,7 +34,8 @@ typedef DWORD			process_t;
 #include <signal.h>
 #include <stdio.h>
 
-typedef pthread_t		thread_t;
+//#include <mach/mach_types.h> typedef mach_port_t thread_t
+//typedef pthread_t		thread_t;
 typedef pid_t			process_t;
 
 #ifndef STDCALL
@@ -47,39 +48,30 @@ extern char** environ;
 #include <memory.h>
 #include <errno.h>
 
-#ifndef IN
-#define IN 
-#endif
-
-#ifndef OUT
-#define OUT
-#endif
+typedef int (STDCALL *thread_proc)(void* param);
 
 //////////////////////////////////////////////////////////////////////////
 ///
 /// thread: Windows CreateThread/Linux pthread
 ///
 //////////////////////////////////////////////////////////////////////////
-typedef int (STDCALL *thread_proc)(IN void* param);
 
-inline int thread_create(OUT thread_t* thread, IN thread_proc func, IN void* param);
-inline int thread_destroy(IN thread_t thread);
-
-// priority: [-15, 15]
-// 0: normal / -15: idle / 15: critical
-inline int thread_getpriority(IN thread_t thread, OUT int* priority);
-inline int thread_setpriority(IN thread_t thread, IN int priority);
-
-inline int thread_self(void);
-inline int thread_getid(IN thread_t thread, OUT int* id);
-inline int thread_isself(IN thread_t thread);
-
-inline int process_create(IN const char* filename, OUT process_t* pid);
-inline int process_kill(IN process_t pid);
-
-inline process_t process_self(void);
-inline int process_selfname(char* name, size_t size);
-inline int process_name(process_t pid, char* name, size_t size);
+//-------------------------------------------------------------------------------------
+// int thread_create(pthread_t* thread, thread_proc func, void* param);
+// int thread_destroy(pthread_t thread);
+// int thread_getpriority(pthread_t thread, int* priority);
+// int thread_setpriority(pthread_t thread, int priority);
+// int thread_self(void);
+// int thread_getid(pthread_t thread, int* id);
+// int thread_isself(pthread_t thread);
+//-------------------------------------------------------------------------------------
+// int process_createve(const char* filename, process_create_param_t *param, process_t* pid);
+// int process_create(const char* filename, process_t* pid);
+// int process_kill(process_t pid);
+// process_t process_self(void);
+// int process_selfname(char* name, size_t size);
+// int process_name(process_t pid, char* name, size_t size);
+//-------------------------------------------------------------------------------------
 
 typedef struct
 {
@@ -99,14 +91,12 @@ typedef struct
 #endif
 } process_create_param_t;
 
-inline int process_createve(IN const char* filename, IN process_create_param_t *param, OUT process_t* pid);
-
 //////////////////////////////////////////////////////////////////////////
 ///
 /// thread: Windows CreateThread/Linux pthread
 ///
 //////////////////////////////////////////////////////////////////////////
-inline int thread_create(OUT thread_t* thread, IN thread_proc func, IN void* param)
+inline int thread_create(pthread_t* thread, thread_proc func, void* param)
 {
 #if defined(OS_WINDOWS)
 	// http://msdn.microsoft.com/en-us/library/windows/desktop/ms682453%28v=vs.85%29.aspx
@@ -125,7 +115,7 @@ inline int thread_create(OUT thread_t* thread, IN thread_proc func, IN void* par
 #endif
 }
 
-inline int thread_destroy(IN thread_t thread)
+inline int thread_destroy(pthread_t thread)
 {
 #if defined(OS_WINDOWS)
 	if(thread.id != GetCurrentThreadId())
@@ -143,7 +133,7 @@ inline int thread_destroy(IN thread_t thread)
 
 // priority: [-15, 15]
 // 0: normal / -15: idle / 15: critical
-inline int thread_getpriority(IN thread_t thread, OUT int* priority)
+inline int thread_getpriority(pthread_t thread, int* priority)
 {
 #if defined(OS_WINDOWS)
 	int r = GetThreadPriority(thread.handle);
@@ -161,7 +151,7 @@ inline int thread_getpriority(IN thread_t thread, OUT int* priority)
 #endif
 }
 
-inline int thread_setpriority(IN thread_t thread, IN int priority)
+inline int thread_setpriority(pthread_t thread, int priority)
 {
 #if defined(OS_WINDOWS)
 	BOOL r = SetThreadPriority(thread.handle, priority);
@@ -184,7 +174,7 @@ inline int thread_self(void)
 #endif
 }
 
-inline int thread_getid(IN thread_t thread, OUT int* id)
+inline int thread_getid(pthread_t thread, int* id)
 {
 #if defined(OS_WINDOWS)
 	*id = thread.id;
@@ -199,16 +189,16 @@ inline int thread_getid(IN thread_t thread, OUT int* id)
 #endif
 }
 
-inline int thread_isself(IN thread_t thread)
+inline int thread_isself(pthread_t thread)
 {
 #if defined(OS_WINDOWS)
 	return thread.id==GetCurrentThreadId() ? 1 : 0;
 #else
-	return pthread_equal(pthread_self(),thread);
+	return pthread_equal(pthread_self(), thread);
 #endif
 }
 
-inline int thread_valid(IN thread_t thread)
+inline int thread_valid(pthread_t thread)
 {
 #if defined(OS_WINDOWS)
 	return 0 != thread.id ? 1 : 0;
@@ -223,7 +213,7 @@ inline int thread_valid(IN thread_t thread)
 /// process
 ///
 //////////////////////////////////////////////////////////////////////////
-inline int process_createve(IN const char* filename, IN process_create_param_t *param, OUT process_t* pid)
+inline int process_createve(const char* filename, process_create_param_t *param, process_t* pid)
 {
 #if defined(OS_WINDOWS)
 	PROCESS_INFORMATION pi;
@@ -276,7 +266,7 @@ inline int process_createve(IN const char* filename, IN process_create_param_t *
 #endif
 }
 
-inline int process_create(IN const char* filename, OUT process_t* pid)
+inline int process_create(const char* filename, process_t* pid)
 {
     process_create_param_t param;
 #if defined(OS_WINDOWS)
@@ -291,7 +281,7 @@ inline int process_create(IN const char* filename, OUT process_t* pid)
     return process_createve(filename, &param, pid);
 }
 
-inline int process_kill(IN process_t pid)
+inline int process_kill(process_t pid)
 {
 #if defined(OS_WINDOWS)
 	HANDLE handle = OpenProcess(PROCESS_TERMINATE, FALSE, pid);
@@ -322,7 +312,7 @@ inline process_t process_self(void)
 #endif
 }
 
-inline int process_selfname(OUT char* name, IN size_t size)
+inline int process_selfname(char* name, size_t size)
 {
 #if defined(OS_WINDOWS)
 	if(0 == GetModuleFileNameA(NULL, name, size))
@@ -337,7 +327,7 @@ inline int process_selfname(OUT char* name, IN size_t size)
 #endif
 }
 
-inline int process_name(IN process_t pid, OUT char* name, IN size_t size)
+inline int process_name(process_t pid, char* name, size_t size)
 {
 #if defined(OS_WINDOWS)
 	DWORD r = 0;
