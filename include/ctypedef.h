771a32999bcf14dc64aa7ee849280f973ff20db9
i guest <iguest@sogou.com>
2014-11-25 14:26:32 +0800

    define OS_INT64_TYPE

--- include/ctypedef.h
+++ include/ctypedef.h
@@ -14,12 +14,18 @@
 	typedef unsigned short		uint16_t;
 	typedef unsigned int		uint32_t;
 
+	#ifndef OS_INT64_TYPE
 	typedef __int64				int64_t;
 	typedef unsigned __int64	uint64_t;
+	#define OS_INT64_TYPE
+	#endif /* OS_INT64_TYPE */
 #else
 	#include <stdint.h>
+	#ifndef OS_INT64_TYPE
 	typedef long long			int64_t;
 	typedef unsigned long long	uint64_t;
+	#define OS_INT64_TYPE
+	#endif /* OS_INT64_TYPE */
 #endif
 
 #endif /* !_ctypedef_h_ */
