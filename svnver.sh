ab12666bfc8037d23ba82bde8e539059ac67faa5
i guest <iguest@sogou.com>
2015-02-02 15:45:43 +0800

    svn 1.6.x / 1.8.x

--- svnver.sh
+++ svnver.sh
@@ -1,6 +1,14 @@
 svninfo=$(svn info -r HEAD)
-lastchangerev=$(printf "%s\n" "$svninfo" | awk 'NR==8' | awk -F ': ' '{print $2}')
-lastchangedate=$(printf "%s\n" "$svninfo" | awk 'NR==9' | awk -F ': ' '{print $2}')
+if [ "11" = $(svn info | wc -l) ]
+then
+# svn 1.6.x
+	lastchangerev=$(printf "%s\n" "$svninfo" | awk 'NR==8' | awk -F ': ' '{print $2}')
+	lastchangedate=$(printf "%s\n" "$svninfo" | awk 'NR==9' | awk -F ': ' '{print $2}')
+else
+# svn 1.8.x
+	lastchangerev=$(printf "%s\n" "$svninfo" | awk 'NR==9' | awk -F ': ' '{print $2}')
+	lastchangedate=$(printf "%s\n" "$svninfo" | awk 'NR==10' | awk -F ': ' '{print $2}')
+fi
 echo "Last Changed Rev:" "$lastchangerev"
 echo "Last Changed Date:" "$lastchangedate"
 sed -e 's/\$WCREV\$/'"$lastchangerev"'/' -e 's/\$WCDATE\$/'"$lastchangedate"'/' -e 's/\$WCNOW\$/'"$(date)"'/' "$1" > "$2"
