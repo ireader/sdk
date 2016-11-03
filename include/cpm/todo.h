473df0a3e58b39c8d3252f67c72b061a0f6e53a9
ireader <ch.magus@163.com>
2016-11-03 16:50:19 +0800

    gcc TODO/FIXME

--- include/cpm/todo.h
+++ include/cpm/todo.h
@@ -8,11 +8,11 @@
 #define __LOC__ __FILE__ "("__STR1__(__LINE__)"): "
 
 #if _MSC_VER
-#define TODO(msg) message(__LOC__ "TODO: " #msg)
-	#define FIXME(msg)
+	#define TODO(msg) message(__LOC__ "TODO: " #msg)
+	#define FIXME(msg) message(__LOC__ "FIXME: " #msg)
 #elif __GNUC__
-	#define TODO(msg)
-	#define FIXME(msg)
+	#define TODO(msg) message(__LOC__ "TODO: " #msg)
+	#define FIXME(msg) message(__LOC__ "FIXME: " #msg)
 #else // XCODE
 	#define TODO(msg)	TODO msg
 	#define FIXME(msg)	FIXME msg
