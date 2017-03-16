46666361dd45e1c0fb9cab111e11e58791ef5f8b
ireader <ch.magus@163.com>
2017-03-16 13:59:21 +0800

    add be_read_uint24/be_write_uint24

--- include/byte-order.h
+++ include/byte-order.h
@@ -81,6 +81,11 @@ static inline void be_read_uint16(const uint8_t* ptr, uint16_t* val)
 	*val = (((uint16_t)ptr[0]) << 8) | ptr[1];
 }
 
+static void be_read_uint24(const uint8_t* ptr, uint32_t* val)
+{
+	*val = (((uint32_t)ptr[0]) << 16) | (((uint32_t)ptr[1]) << 8) | ptr[2];
+}
+
 static inline void be_read_uint32(const uint8_t* ptr, uint32_t* val)
 {
 	*val = (((uint32_t)ptr[0]) << 24) | (((uint32_t)ptr[1]) << 16) | (((uint32_t)ptr[2]) << 8) | ptr[3];
@@ -98,6 +103,13 @@ static inline void be_write_uint16(uint8_t* ptr, uint16_t val)
 {
 	ptr[0] = (uint8_t)((val >> 8) & 0xFF);
 	ptr[1] = (uint8_t)(val & 0xFF);
+}
+
+static inline void be_write_uint24(uint8_t* ptr, uint32_t val)
+{
+	ptr[0] = (uint8_t)((val >> 16) & 0xFF);
+	ptr[1] = (uint8_t)((val >> 8) & 0xFF);
+	ptr[2] = (uint8_t)(val & 0xFF);
 }
 
 static inline void be_write_uint32(uint8_t* ptr, uint32_t val)
