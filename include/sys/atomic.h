dc3df5ad73040972c2c71901562036078dfb97a6
ireader <ch.magus@163.com>
2017-05-19 19:43:59 +0800

    replace ptrdiff_t with intptr_t

--- include/sys/atomic.h
+++ include/sys/atomic.h
@@ -35,28 +35,28 @@ typedef __int64 int64_t;
 #if defined(OS_WINDOWS)
 static inline int32_t atomic_increment32(volatile int32_t *value)
 {
-	assert((ptrdiff_t)value % 4 == 0);
+	assert((intptr_t)value % 4 == 0);
 	assert(sizeof(LONG) == sizeof(int32_t));
 	return InterlockedIncrement((LONG volatile*)value);
 }
 
 static inline int32_t atomic_decrement32(volatile int32_t *value)
 {
-	assert((ptrdiff_t)value % 4 == 0);
+	assert((intptr_t)value % 4 == 0);
 	assert(sizeof(LONG) == sizeof(int32_t));
 	return InterlockedDecrement((LONG volatile*)value);
 }
 
 static inline int32_t atomic_add32(volatile int32_t *value, int32_t incr)
 {
-	assert((ptrdiff_t)value % 4 == 0);
+	assert((intptr_t)value % 4 == 0);
 	assert(sizeof(LONG) == sizeof(int32_t));
 	return InterlockedExchangeAdd((LONG volatile*)value, incr) + incr; // The function returns the initial value of the Addend parameter.
 }
 
 static inline int atomic_cas32(volatile int32_t *value, int32_t oldvalue, int32_t newvalue)
 {
-	assert((ptrdiff_t)value % 4 == 0);
+	assert((intptr_t)value % 4 == 0);
 	assert(sizeof(LONG) == sizeof(int32_t));
 	return oldvalue == InterlockedCompareExchange((LONG volatile*)value, newvalue, oldvalue) ? 1 : 0;
 }
@@ -64,9 +64,9 @@ static inline int atomic_cas32(volatile int32_t *value, int32_t oldvalue, int32_
 static inline int atomic_cas_ptr(void* volatile *value, void *oldvalue, void *newvalue)
 {
 #if defined(_WIN64) || defined(WIN64)
-	assert(0 == (ptrdiff_t)value % 8 && 0 == (ptrdiff_t)oldvalue % 8 && 0 == (ptrdiff_t)newvalue % 8);
+	assert(0 == (intptr_t)value % 8 && 0 == (intptr_t)oldvalue % 8 && 0 == (intptr_t)newvalue % 8);
 #else
-	assert(0 == (ptrdiff_t)value % 4 && 0 == (ptrdiff_t)oldvalue % 4 && 0 == (ptrdiff_t)newvalue % 4);
+	assert(0 == (intptr_t)value % 4 && 0 == (intptr_t)oldvalue % 4 && 0 == (intptr_t)newvalue % 4);
 #endif
 	return oldvalue == InterlockedCompareExchangePointer(value, newvalue, oldvalue) ? 1 : 0;
 }
@@ -75,28 +75,28 @@ static inline int atomic_cas_ptr(void* volatile *value, void *oldvalue, void *ne
 
 static inline int64_t atomic_increment64(volatile int64_t *value)
 {
-	assert((ptrdiff_t)value % 8 == 0);
+	assert((intptr_t)value % 8 == 0);
 	assert(sizeof(LONGLONG) == sizeof(int64_t));
 	return InterlockedIncrement64(value);
 }
 
 static inline int64_t atomic_decrement64(volatile int64_t *value)
 {
-	assert((ptrdiff_t)value % 8 == 0);
+	assert((intptr_t)value % 8 == 0);
 	assert(sizeof(LONGLONG) == sizeof(int64_t));
 	return InterlockedDecrement64(value);
 }
 
 static inline int64_t atomic_add64(volatile int64_t *value, int64_t incr)
 {
-	assert((ptrdiff_t)value % 8 == 0);
+	assert((intptr_t)value % 8 == 0);
 	assert(sizeof(LONGLONG) == sizeof(int64_t));
 	return InterlockedExchangeAdd64(value, incr) + incr; // The function returns the initial value of the Addend parameter.
 }
 
 static inline int atomic_cas64(volatile int64_t *value, int64_t oldvalue, int64_t newvalue)
 {
-	assert((ptrdiff_t)value % 8 == 0);
+	assert((intptr_t)value % 8 == 0);
 	assert(sizeof(LONGLONG) == sizeof(int64_t));
 	return oldvalue == InterlockedCompareExchange64(value, newvalue, oldvalue) ? 1 : 0;
 }
@@ -107,58 +107,58 @@ static inline int atomic_cas64(volatile int64_t *value, int64_t oldvalue, int64_
 #elif defined(OS_MAC)
 static inline int32_t atomic_increment32(volatile int32_t *value)
 {
-	assert((ptrdiff_t)value % 4 == 0);
+	assert((intptr_t)value % 4 == 0);
 	return OSAtomicIncrement32(value);
 }
 
 static inline int64_t atomic_increment64(volatile int64_t *value)
 {
-	assert((ptrdiff_t)value % 8 == 0);
+	assert((intptr_t)value % 8 == 0);
 	return OSAtomicIncrement64(value);
 }
 
 static inline int32_t atomic_decrement32(volatile int32_t *value)
 {
-	assert((ptrdiff_t)value % 4 == 0);
+	assert((intptr_t)value % 4 == 0);
 	return OSAtomicDecrement32(value);
 }
 
 static inline int64_t atomic_decrement64(volatile int64_t *value)
 {
-	assert((ptrdiff_t)value % 8 == 0);
+	assert((intptr_t)value % 8 == 0);
 	return OSAtomicDecrement64(value);
 }
 
 static inline int32_t atomic_add32(volatile int32_t *value, int32_t incr)
 {
-	assert((ptrdiff_t)value % 4 == 0);
+	assert((intptr_t)value % 4 == 0);
 	return OSAtomicAdd32(incr, value);
 }
 
 static inline int64_t atomic_add64(volatile int64_t *value, int64_t incr)
 {
-	assert((ptrdiff_t)value % 8 == 0);
+	assert((intptr_t)value % 8 == 0);
 	return OSAtomicAdd64(incr, value);
 }
 
 static inline int atomic_cas32(volatile int32_t *value, int32_t oldvalue, int32_t newvalue)
 {
-	assert((ptrdiff_t)value % 4 == 0);
+	assert((intptr_t)value % 4 == 0);
 	return OSAtomicCompareAndSwap32(oldvalue, newvalue, value) ? 1 : 0;
 }
 
 static inline int atomic_cas64(volatile int64_t *value, int64_t oldvalue, int64_t newvalue)
 {
-	assert((ptrdiff_t)value % 8 == 0);
+	assert((intptr_t)value % 8 == 0);
 	return OSAtomicCompareAndSwap64(oldvalue, newvalue, value) ? 1 : 0;
 }
 
 static inline int atomic_cas_ptr(void* volatile *value, void *oldvalue, void *newvalue)
 {
 #if TARGET_CPU_X86_64 || TARGET_CPU_PPC64
-	assert(0 == (ptrdiff_t)value % 8 && 0 == (ptrdiff_t)oldvalue % 8 && 0 == (ptrdiff_t)newvalue % 8);
+	assert(0 == (intptr_t)value % 8 && 0 == (intptr_t)oldvalue % 8 && 0 == (intptr_t)newvalue % 8);
 #else
-	assert(0 == (ptrdiff_t)value % 4 && 0 == (ptrdiff_t)oldvalue % 4 && 0 == (ptrdiff_t)newvalue % 4);
+	assert(0 == (intptr_t)value % 4 && 0 == (intptr_t)oldvalue % 4 && 0 == (intptr_t)newvalue % 4);
 #endif
 	return OSAtomicCompareAndSwapPtr(oldvalue, newvalue, value) ? 1 : 0;
 }
@@ -173,55 +173,55 @@ static inline int atomic_cas_ptr(void* volatile *value, void *oldvalue, void *ne
 #if __GNUC__>=4 && __GNUC_MINOR__>=1
 static inline int32_t atomic_increment32(volatile int32_t *value)
 {
-	assert((ptrdiff_t)value % 4 == 0);
+	assert((intptr_t)value % 4 == 0);
 	return __sync_add_and_fetch_4(value, 1);
 }
 
 static inline int32_t atomic_decrement32(volatile int32_t *value)
 {
-	assert((ptrdiff_t)value % 4 == 0);
+	assert((intptr_t)value % 4 == 0);
 	return __sync_sub_and_fetch_4(value, 1);
 }
 
 static inline int32_t atomic_add32(volatile int32_t *value, int32_t incr)
 {
-	assert((ptrdiff_t)value % 4 == 0);
+	assert((intptr_t)value % 4 == 0);
 	return __sync_add_and_fetch_4(value, incr);
 }
 
 static inline int atomic_cas32(volatile int32_t *value, int32_t oldvalue, int32_t newvalue)
 {
-	assert((ptrdiff_t)value % 4 == 0);
+	assert((intptr_t)value % 4 == 0);
 	return __sync_bool_compare_and_swap_4(value, oldvalue, newvalue) ? 1 : 0;
 }
 
 static inline int atomic_cas_ptr(void* volatile *value, void *oldvalue, void *newvalue)
 {
-	assert(0 == (ptrdiff_t)value % 4 && 0 == (ptrdiff_t)oldvalue % 4 && 0 == (ptrdiff_t)newvalue % 4);
+	assert(0 == (intptr_t)value % 4 && 0 == (intptr_t)oldvalue % 4 && 0 == (intptr_t)newvalue % 4);
 	return __sync_bool_compare_and_swap(value, oldvalue, newvalue);
 }
 
 static inline int64_t atomic_increment64(volatile int64_t *value)
 {
-	assert((ptrdiff_t)value % 8 == 0);
+	assert((intptr_t)value % 8 == 0);
 	return __sync_add_and_fetch_8(value, 1);
 }
 
 static inline int64_t atomic_decrement64(volatile int64_t *value)
 {
-	assert((ptrdiff_t)value % 8 == 0);
+	assert((intptr_t)value % 8 == 0);
 	return __sync_sub_and_fetch_8(value, 1);
 }
 
 static inline int64_t atomic_add64(volatile int64_t *value, int64_t incr)
 {
-	assert((ptrdiff_t)value % 8 == 0);
+	assert((intptr_t)value % 8 == 0);
 	return __sync_add_and_fetch_8(value, incr);
 }
 
 static inline int atomic_cas64(volatile int64_t *value, int64_t oldvalue, int64_t newvalue)
 {
-	assert((ptrdiff_t)value % 8 == 0);
+	assert((intptr_t)value % 8 == 0);
 	return __sync_bool_compare_and_swap_8(value, oldvalue, newvalue) ? 1 : 0;
 }
 
@@ -230,7 +230,7 @@ static inline int atomic_cas64(volatile int64_t *value, int64_t oldvalue, int64_
 
 static inline int32_t atomic_add32(volatile int32_t *value, int32_t incr)
 {
-	assert((ptrdiff_t)value % 4 == 0);
+	assert((intptr_t)value % 4 == 0);
 	int a, b, c;
 	asm volatile(	"0:\n\t"
 					"ldr %0, [%3]\n\t"
@@ -247,13 +247,13 @@ static inline int32_t atomic_add32(volatile int32_t *value, int32_t incr)
 
 static inline int32_t atomic_increment32(volatile int32_t *value)
 {
-	assert((ptrdiff_t)value % 4 == 0);
+	assert((intptr_t)value % 4 == 0);
 	return atomic_add32(value, 1);
 }
 
 static inline int32_t atomic_decrement32(volatile int32_t *value)
 {
-	assert((ptrdiff_t)value % 4 == 0);
+	assert((intptr_t)value % 4 == 0);
 	return atomic_add32(value, -1);
 }
 
@@ -262,7 +262,7 @@ static inline int32_t atomic_decrement32(volatile int32_t *value)
 static inline int32_t atomic_add32(volatile int32_t *value, int32_t incr)
 {
 	int32_t r = incr;
-	assert((ptrdiff_t)value % 4 == 0);
+	assert((intptr_t)value % 4 == 0);
 	asm volatile ("lock; xaddl %0,%1"
 		: "+r" (r), "+m" (*value));
 	return r + incr;
@@ -270,20 +270,20 @@ static inline int32_t atomic_add32(volatile int32_t *value, int32_t incr)
 
 static inline int32_t atomic_increment32(volatile int32_t *value)
 {
-	assert((ptrdiff_t)value % 4 == 0);
+	assert((intptr_t)value % 4 == 0);
 	return atomic_add32(value, 1);
 }
 
 static inline int32_t atomic_decrement32(volatile int32_t *value)
 {
-	assert((ptrdiff_t)value % 4 == 0);
+	assert((intptr_t)value % 4 == 0);
 	return atomic_add32(value, -1);
 }
 
 static inline int atomic_cas32(volatile int32_t *value, int32_t oldvalue, int32_t newvalue)
 {
 	int32_t prev = oldvalue;
-	assert((ptrdiff_t)value % 4 == 0);
+	assert((intptr_t)value % 4 == 0);
 	asm volatile ("lock; cmpxchgl %2, %1"
 		: "+a" (prev), "+m" (*value)
 		: "r" (newvalue));
@@ -294,7 +294,7 @@ static inline int atomic_cas64(volatile int64_t *value, int64_t oldvalue, int64_
 {
 	int64_t prev = oldvalue;
 #if defined(__LP64__)
-	assert((ptrdiff_t)value % 8 == 0);
+	assert((intptr_t)value % 8 == 0);
 	asm volatile ("lock ; cmpxchgq %2, %1"
 		: "+a" (prev), "+m" (*value)
 		: "q" (newvalue));
@@ -319,7 +319,7 @@ static inline int64_t atomic_add64(volatile int64_t *value, int64_t incr)
 {
 #if defined(__LP64__)
 	int64_t r = incr;
-	assert((ptrdiff_t)value % 8 == 0);
+	assert((intptr_t)value % 8 == 0);
 	asm volatile ("lock; xaddq %0,%1"
 		: "+r" (r), "+m" (*value));
 	return r + incr;
@@ -335,13 +335,13 @@ static inline int64_t atomic_add64(volatile int64_t *value, int64_t incr)
 
 static inline int64_t atomic_increment64(volatile int64_t *value)
 {
-	assert((ptrdiff_t)value % 8 == 0);
+	assert((intptr_t)value % 8 == 0);
 	return atomic_add64(value, 1);
 }
 
 static inline int64_t atomic_decrement64(volatile int64_t *value)
 {
-	assert((ptrdiff_t)value % 8 == 0);
+	assert((intptr_t)value % 8 == 0);
 	return atomic_add64(value, -1);
 }
 
