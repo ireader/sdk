b17871f15ae66e08917884babaee744e51596c80
ireader <tao3@outlook.com>
2017-07-27 10:48:08 +0800

    add aio_connect test

---
+++ libaio/test/aio-connect-test.cpp
@@ -0,0 +1,36 @@
+#include "aio-connect.h"
+#include "aio-timeout.h"
+#include "aio-socket.h"
+#include "sys/system.h"
+#include <stdio.h>
+
+static void aio_connect_baidu(void* param, aio_socket_t aio, int code)
+{
+	uint64_t now = system_clock();
+	uint64_t* clock = (uint64_t*)param;
+	printf("connect baidu: %d, time: %d\n", code, (int)(now - *clock));
+}
+
+static void aio_connect_google(void* param, aio_socket_t aio, int code)
+{
+	uint64_t now = system_clock();
+	uint64_t* clock = (uint64_t*)param;
+	printf("connect google: %d, time: %d\n", code, (int)(now - *clock));
+}
+
+void aio_connect_test()
+{
+	aio_socket_init(1);
+
+	uint64_t clock = system_clock();
+	aio_connect("www.baidu.com", 80, 3000, aio_connect_baidu, &clock);
+	aio_connect("www.google.com", 80, 3000, aio_connect_google, &clock);
+
+	for(int i = 0; i < 10; i++)
+	{
+		aio_socket_process(1000);
+		aio_timeout_process();
+	}
+
+	aio_socket_clean();
+}
