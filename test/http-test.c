3e5bbf82aff795c8b2bf42b5f4564a34ac22d681
ireader <tao3@outlook.com>
2017-08-31 15:07:40 +0800

    add http_header_range_test

--- test/http-test.c
+++ test/http-test.c
@@ -4,6 +4,7 @@ void http_cookie_test(void);
 void http_request_test(void);
 void http_header_host_test(void);
 void http_header_content_type_test(void);
+void http_header_range_test(void);
 
 void http_test(void)
 {
@@ -11,4 +12,5 @@ void http_test(void)
 	http_request_test();
 	http_header_host_test();
 	http_header_content_type_test();
+	http_header_range_test();
 }
