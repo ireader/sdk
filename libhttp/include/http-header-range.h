de7e914aeb637cf4b4bc8e38e586cb57f86a8d53
ireader <tao3@outlook.com>
2017-08-31 15:04:35 +0800

    add HTTP Header Range parser

---
+++ libhttp/include/http-header-range.h
@@ -0,0 +1,38 @@
+#ifndef _http_header_range_h_
+#define _http_header_range_h_
+
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+struct http_header_range_t
+{
+	// The first-byte-pos value in a byte-range-spec gives the byte-offset of the first byte in a range
+	// Byte offsets start at zero.
+	int64_t start;
+
+	// The last-byte-pos value gives the byte-offset of the last byte in the range; 
+	// that is, the byte positions specified are inclusive
+	// if start == -1, used to specify the suffix of the entity-body.
+	// If the entity is shorter than the specified suffix-length, the entire entity-body is used.
+	int64_t end;
+};
+
+/*
+Examples of byte-ranges-specifier values (assuming an entity-body of length 10000):
+1. The first 500 bytes (byte offsets 0-499, inclusive): bytes=0-499
+2. The second 500 bytes (byte offsets 500-999, inclusive): bytes=500-999
+3. The final 500 bytes (byte offsets 9500-9999, inclusive): bytes=-500 Or bytes=9500-
+4. The first and last bytes only (bytes 0 and 9999): bytes=0-0,-1
+5. Several legal but not canonical specifications of the second 500 bytes (byte offsets 500-999, inclusive):
+	bytes=500-600,601-999
+	bytes=500-700,601-999
+*/
+int http_header_range(const char* field, struct http_header_range_t* range, size_t num);
+
+#ifdef __cplusplus
+}
+#endif
+#endif /* !_http_header_range_h_ */
---
+++ libhttp/source/http-header-range.c
@@ -0,0 +1,79 @@
+#include "http-header-range.h"
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+
+static inline const char* string_token_int64(const char* str, int64_t *value)
+{
+	*value = 0;
+	while ('0' <= *str && *str <= '9')
+	{
+		*value = (*value * 10) + (*str - '0');
+		++str;
+	}
+	return str;
+}
+
+int http_header_range(const char* field, struct http_header_range_t* range, size_t num)
+{
+	size_t i;
+	const char* p;
+
+	p = field;
+	for (i = 0; i < num && p; i++)
+	{
+		p = strpbrk(p, "0123456789-\r\n");
+		if (NULL == p || '\r' == *p || '\n' == *p)
+			return i;
+
+		if ('-' == *p)
+		{
+			range[i].start = -1;
+		}
+		else
+		{
+			p = string_token_int64(p, &range[i].start);
+		}
+
+		p = strpbrk(p, "0123456789,\r\n");
+		if (NULL == p || '\r' == *p || '\n' == *p || ',' == *p)
+		{
+			if (-1 == range[i].start)
+				return -1; // invalid
+			range[i].end = -1;
+		}
+		else
+		{
+			p = string_token_int64(p, &range[i].end);
+		}
+	}
+
+	return p ? -1 : i;
+}
+
+#if defined(DEBUG) || defined(_DEBUG)
+void http_header_range_test(void)
+{
+	struct http_header_range_t range[3];
+
+	assert(1 == http_header_range("bytes=0-499", range, 3));
+	assert(0 == range[0].start && 499 == range[0].end);
+
+	assert(1 == http_header_range("bytes=500-999", range, 3));
+	assert(500 == range[0].start && 999 == range[0].end);
+
+	assert(1 == http_header_range("bytes=-500", range, 3));
+	assert(-1 == range[0].start && 500 == range[0].end);
+
+	assert(1 == http_header_range("bytes=9500-", range, 3));
+	assert(9500 == range[0].start && -1 == range[0].end);
+
+	assert(2 == http_header_range("bytes=0-0,-1", range, 3));
+	assert(0 == range[0].start && 0 == range[0].end);
+	assert(-1 == range[1].start && 1 == range[1].end);
+
+	assert(2 == http_header_range("bytes=500-600,601-999", range, 3));
+	assert(500 == range[0].start && 600 == range[0].end);
+	assert(601 == range[1].start && 999 == range[1].end);
+}
+#endif
