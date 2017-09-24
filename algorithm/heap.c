fc051671217a8192e3a929476926124fd3769aca
ireader <tao3@outlook.com>
2017-09-24 13:28:49 +0800

    min-heap

---
+++ algorithm/heap.c
@@ -0,0 +1,183 @@
+// https://en.wikipedia.org/wiki/Heap_(data_structure)
+// https://en.wikipedia.org/wiki/Binary_heap
+// https://en.wikipedia.org/wiki/Fibonacci_heap
+
+#include "heap.h"
+#include <errno.h>
+#include <stdlib.h>
+#include <assert.h>
+
+struct heap_t
+{
+	void** elts; // elements
+	int size;
+	int capacity;
+
+	heap_less less;
+};
+
+static void heap_percolate_up(struct heap_t* heap);
+static void heap_percolate_down(struct heap_t* heap);
+
+struct heap_t* heap_create(heap_less compare)
+{
+	struct heap_t* heap;
+	heap = (struct heap_t*)calloc(1, sizeof(*heap));
+	if (heap)
+	{
+		heap->less = compare;
+	}
+	return heap;
+}
+
+void heap_destroy(struct heap_t* heap)
+{
+	if (heap->elts)
+	{
+		assert(heap->capacity > 0);
+		free(heap->elts);
+		heap->elts = NULL;
+	}
+
+	free(heap);
+}
+
+void heap_reserve(struct heap_t* heap, int size)
+{
+	void* p;
+	if (heap->capacity < size)
+	{
+		p = realloc(heap->elts, sizeof(void*) * size);
+		if (p)
+		{
+			heap->elts = (void**)p;
+			heap->capacity = size;
+		}
+	}
+}
+
+int heap_size(struct heap_t* heap)
+{
+	return heap->size;
+}
+
+int heap_empty(struct heap_t* heap)
+{
+	return !heap->size;
+}
+
+int heap_push(struct heap_t* heap, void* ptr)
+{
+	if (heap->size + 1 >= heap->capacity)
+	{
+		heap_reserve(heap, heap->size + 2014);
+		if (heap->size + 1 >= heap->capacity)
+			return ENOMEM;
+	}
+
+	heap->elts[heap->size++] = ptr;
+	heap_percolate_up(heap);
+	return 0;
+}
+
+void heap_pop(struct heap_t* heap)
+{
+	if (heap->size < 1)
+		return;
+	heap_percolate_down(heap);
+	heap->size -= 1;
+}
+
+void* heap_top(struct heap_t* heap)
+{
+	if (heap->size < 1)
+		return NULL;
+	return heap->elts[0];
+}
+
+static inline void heap_swap(struct heap_t* heap, int l, int r)
+{
+	void* elt;
+	elt = heap->elts[l];
+	heap->elts[l] = heap->elts[r];
+	heap->elts[r] = elt;
+}
+
+static void heap_percolate_up(struct heap_t* heap)
+{
+	int n, p;
+
+	assert(heap->size > 0);
+	for (n = heap->size - 1; n > 0; n = p)
+	{
+		p = (n - 1) / 2; // parent
+		if (heap->less(heap->elts[p], heap->elts[n]))
+			break;
+
+		heap_swap(heap, p, n);
+	}
+}
+
+static void heap_percolate_down(struct heap_t* heap)
+{
+	int n, c;
+	assert(heap->size > 0);
+
+	// replace first with last
+	heap->elts[0] = heap->elts[heap->size - 1];
+
+	for (n = 0; n < heap->size - 1; n = c)
+	{
+		// left/right child
+		for (c = n * 2 + 1; c < heap->size - 1 && c < n * 2 + 3; c++)
+		{
+			if (heap->less(heap->elts[c], heap->elts[n]))
+			{
+				heap_swap(heap, c, n);
+				break;
+			}
+		}
+
+		if(c == n * 2 + 3)
+			break;
+	}
+}
+
+#if defined(_DEBUG) || defined(DEBUG)
+#include <stdio.h>
+#include <time.h>
+#define N 10000
+static int heap_test_compare(void* p1, void* p2)
+{
+	return *(int*)p1 < *(int*)p2;
+}
+
+void heap_test(void)
+{
+	int i;
+	int *n, *v;
+	heap_t* heap;
+
+	v = malloc(sizeof(int) * N);
+	srand((unsigned int)time(NULL));
+
+	heap = heap_create(heap_test_compare);
+	for (i = 0; i < N; i++)
+	{
+		v[i] = rand();
+		heap_push(heap, &v[i]);
+	}
+	assert(heap_size(heap) == N);
+
+	n = (int*)heap_top(heap); // first value
+	heap_pop(heap);
+	for (i = 1; i < N; i++)
+	{
+		assert(*n <= *(int*)heap_top(heap));
+		heap_pop(heap);
+	}
+	assert(heap_empty(heap));
+	heap_destroy(heap);
+	printf("heap test ok\n");
+}
+#endif
---
+++ algorithm/heap.h
@@ -0,0 +1,33 @@
+#ifndef _heap_h_
+#define _heap_h_
+
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
+typedef struct heap_t heap_t;
+
+/// heap compare callback
+/// @return 1 if ptr1 < ptr2, 0-other
+typedef int (*heap_less)(void* ptr1, void* ptr2);
+
+/// create heap
+/// default min-heap, change heap_less behavor to create max-heap
+heap_t* heap_create(heap_less compare);
+void heap_destroy(heap_t* heap);
+
+/// reserve heap capacity
+/// if size <= capacity, do nothing
+void heap_reserve(heap_t* heap, int size);
+
+int heap_size(heap_t* heap);
+int heap_empty(heap_t* heap);
+
+int heap_push(heap_t* heap, void* ptr);
+void heap_pop(heap_t* heap);
+void* heap_top(heap_t* heap);
+
+#if defined(__cplusplus)
+}
+#endif
+#endif /* !_heap_h_ */
