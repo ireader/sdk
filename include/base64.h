301564fe9876dc9426834a3528970e2ad1c12b76
ireader <tao3@outlook.com>
2017-09-29 16:24:59 +0800

    add base16_encode/base16_decode

--- include/base64.h
+++ include/base64.h
@@ -8,18 +8,25 @@ extern "C" {
 #endif
 
 /// base64 encode
+/// @param[out] target output string buffer, target size = (source + 2)/3 * 4 + source / 57 (76/4*3)
 /// @param[in] source input binary buffer
 /// @param[in] bytes input buffer size in byte
-/// @param[out] target output string buffer, target size = (source + 2)/3 * 4 + source / 57 (76/4*3)
-/// @return output buffer bytes(add '\n' per 76 bytes, see more RFC2045 page 25)
+/// @return target bytes
 size_t base64_encode(char* target, const void *source, size_t bytes);
+/// same as base64_encode except "+/" -> "-_"
+size_t base64_encode_url(char* target, const void *source, size_t bytes);
 
 /// base64 decode
 /// @param[in] source input string buffer
 /// @param[in] bytes input buffer size in byte
 /// @param[out] target output binary buffer, target size = (source + 3)/4 * 3;
-/// @return output buffer bytes
+/// @return target bytes
 size_t base64_decode(void* target, const char *source, size_t bytes);
+
+/// Base16(HEX) Encoding
+/// @return target bytes
+size_t base16_encode(char* target, const void *source, size_t bytes);
+size_t base16_decode(void* target, const char *source, size_t bytes);
 
 #ifdef  __cplusplus
 } // extern "C"
--- source/base64.c
+++ source/base64.c
@@ -1,119 +1,142 @@
 #include "base64.h"
 #include <assert.h>
+#include <stdint.h>
 
-static char encode_table[64] = {
+static char s_base64_enc[64] = {
 	'A','B','C','D','E','F','G','H','I','J','K','L','M',
 	'N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
 	'a','b','c','d','e','f','g','h','i','j','k','l','m',
 	'n','o','p','q','r','s','t','u','v','w','x','y','z',
 	'0','1','2','3','4','5','6','7','8','9','+','/'
 };
+static char s_base64_url[64] = {
+	'A','B','C','D','E','F','G','H','I','J','K','L','M',
+	'N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
+	'a','b','c','d','e','f','g','h','i','j','k','l','m',
+	'n','o','p','q','r','s','t','u','v','w','x','y','z',
+	'0','1','2','3','4','5','6','7','8','9','-','_'
+};
+static const uint8_t s_base64_dec[128] = {
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,62, 0,62, 0,63,  /* +/-/ */
+	52,53,54,55,56,57,58,59,60,61, 0, 0, 0, 0, 0, 0, /* 0 - 9 */
+	0, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9,10,11,12,13,14,  /* A - Z */
+	15,16,17,18,19,20,21,22,23,24,25, 0, 0, 0, 0,63, /* _ */
+	00,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40, /* a - z */
+	41,42,43,44,45,46,47,48,49,50,51, 0, 0, 0, 0, 0,
+};
 
-static unsigned char decode_table[256];
-
-size_t base64_encode(char* target, const void *source, size_t bytes)
+static size_t base64_encode_table(char* target, const void *source, size_t bytes, const char* table)
 {
-#define ENC(c) encode_table[c]
+	size_t i, j;
+	const uint8_t *ptr = (const uint8_t*)source;
 
-	size_t i, j=0;
-	size_t n3bytes = bytes/3*3;
-    const unsigned char *ptr = (const unsigned char*)source;
-
-    for (i = 0; i < n3bytes; i += 3) {
-        target[j++] = ENC((ptr[i] >> 2) & 077);            /* c1 */
-        target[j++] = ENC(((ptr[i] << 4) & 060) | ((ptr[i+1] >> 4) & 017)); /*c2*/
-        target[j++] = ENC(((ptr[i+1] << 2) & 074) | ((ptr[i+2] >> 6) & 03));/*c3*/
-        target[j++] = ENC(ptr[i+2] & 077);         /* c4 */
-
-        // add '\n' per 76 bytes
-        // 54 = 76 / 4 * 3
-        //if(i > 0 && 0 == (i % 57)) target[j++] = '\n'; // RFC-2045 p25
+	for (j = i = 0; i < bytes / 3 * 3; i += 3)
+	{
+		target[j++] = table[(ptr[i] >> 2) & 0x3F]; /* c1 */
+		target[j++] = table[((ptr[i] & 0x03) << 4) | ((ptr[i + 1] >> 4) & 0x0F)]; /*c2*/
+		target[j++] = table[((ptr[i + 1] & 0x0F) << 2) | ((ptr[i + 2] >> 6) & 0x03)];/*c3*/
+		target[j++] = table[ptr[i + 2] & 0x3F]; /* c4 */
 	}
 
-	if(bytes >= n3bytes+1) {
+	if (i < bytes)
+	{
 		/* There were only 2 bytes in that last group */
-		target[j++] = ENC((ptr[i] >> 2) & 077);
+		target[j++] = table[(ptr[i] >> 2) & 0x3F];
 
-		if(bytes < n3bytes+2) {
+		if (i + 1 < bytes)
+		{
+			target[j++] = table[((ptr[i] & 0x03) << 4) | ((ptr[i + 1] >> 4) & 0x0F)]; /*c2*/
+			target[j++] = table[((ptr[i + 1] & 0x0F) << 2)]; /*c3*/
+		}
+		else
+		{
 			/* There was only 1 byte in that last group */
-			target[j++] = ENC(((ptr[i] << 4) & 060)); /*c2*/
+			target[j++] = table[((ptr[i] & 0x03) << 4)]; /*c2*/
 			target[j++] = '='; /*c3*/
 		}
-		else{
-			target[j++] = ENC(((ptr[i] << 4) & 060) | ((ptr[i+1] >> 4) & 017)); /*c2*/
-			target[j++] = ENC(((ptr[i+1] << 2) & 074));/*c3*/
-		}
 
-        target[j++] = '='; /*c4*/
-	} 
+		target[j++] = '='; /*c4*/
+	}
 
 	return j;
 }
 
+size_t base64_encode(char* target, const void *source, size_t bytes)
+{
+	return base64_encode_table(target, source, bytes, s_base64_enc);
+}
+
+size_t base64_encode_url(char* target, const void *source, size_t bytes)
+{
+	return base64_encode_table(target, source, bytes, s_base64_url);
+}
+
 size_t base64_decode(void* target, const char *source, size_t bytes)
 {
-	/* single character decode */
-#define DEC(c) decode_table[(int)c]
-#define MAXVAL 63
-
-	static int first = 1;
-
 	size_t i, j;
-	register const unsigned char *bufin = (const unsigned char*)source;
-	register unsigned char *bufout = (unsigned char*)target;
-
-	/* If this is the first call, initialize the mapping table.
-	* This code should work even on non-ASCII machines.
-	*/
-	if(first) {
-		first = 0;
-		for(j=0; j<256; j++) decode_table[j] = 0;
-
-		for(j=0; j<64; j++) decode_table[(int)encode_table[j]] = (unsigned char) j;
-#if 0
-		pr2six['A']= 0; pr2six['B']= 1; pr2six['C']= 2; pr2six['D']= 3; 
-		pr2six['E']= 4; pr2six['F']= 5; pr2six['G']= 6; pr2six['H']= 7; 
-		pr2six['I']= 8; pr2six['J']= 9; pr2six['K']=10; pr2six['L']=11; 
-		pr2six['M']=12; pr2six['N']=13; pr2six['O']=14; pr2six['P']=15; 
-		pr2six['Q']=16; pr2six['R']=17; pr2six['S']=18; pr2six['T']=19; 
-		pr2six['U']=20; pr2six['V']=21; pr2six['W']=22; pr2six['X']=23; 
-		pr2six['Y']=24; pr2six['Z']=25; pr2six['a']=26; pr2six['b']=27; 
-		pr2six['c']=28; pr2six['d']=29; pr2six['e']=30; pr2six['f']=31; 
-		pr2six['g']=32; pr2six['h']=33; pr2six['i']=34; pr2six['j']=35; 
-		pr2six['k']=36; pr2six['l']=37; pr2six['m']=38; pr2six['n']=39; 
-		pr2six['o']=40; pr2six['p']=41; pr2six['q']=42; pr2six['r']=43; 
-		pr2six['s']=44; pr2six['t']=45; pr2six['u']=46; pr2six['v']=47; 
-		pr2six['w']=48; pr2six['x']=49; pr2six['y']=50; pr2six['z']=51; 
-		pr2six['0']=52; pr2six['1']=53; pr2six['2']=54; pr2six['3']=55; 
-		pr2six['4']=56; pr2six['5']=57; pr2six['6']=58; pr2six['7']=59; 
-		pr2six['8']=60; pr2six['9']=61; pr2six['+']=62; pr2six['/']=63;
-#endif
-	}
+	uint8_t* p = (uint8_t*)target;
+	const char* end;
 
 	assert(0 == bytes % 4);
-
-    i = j = 0;
-	for(j=0; j+4 < bytes; j += 4){
-		bufout[i++] = (DEC(bufin[j+0]) << 2) | (DEC(bufin[j+1]) >> 4);
-		bufout[i++] = (DEC(bufin[j+1]) << 4) | (DEC(bufin[j+2]) >> 2);
-		bufout[i++] = (DEC(bufin[j+2]) << 6) | DEC(bufin[j+3]);
-
-        //if(0 == (i % 57))
-        //{
-        //    assert('\n' == bufin[j+4] || '\r' == bufin[j+4]);
-        //    if('\n' == bufin[j+4] || '\r' == bufin[j+4])
-        //        ++j;
-        //    if('\n' == bufin[j+4] || '\r' == bufin[j+4])
-        //        ++j;
-        //}
+	
+	i = 0;
+	end = source + bytes;
+	for (j = 1; j < bytes / 4; j++)
+	{
+		p[i++] = (s_base64_dec[source[0]] << 2) | (s_base64_dec[source[1]] >> 4);
+		p[i++] = (s_base64_dec[source[1]] << 4) | (s_base64_dec[source[2]] >> 2);
+		p[i++] = (s_base64_dec[source[2]] << 6) | s_base64_dec[source[3]];
+		source += 4;
 	}
 
-	// save memory(decode target buffer size = encode source buffer size)
-	if(j < bytes)
+	if (source + 4 == end)
 	{
-		assert(j+4 == bytes); bufout[i++] = (DEC(bufin[j+0]) << 2) | (DEC(bufin[j+1]) >> 4);
-		if('=' != bufin[j+2]) bufout[i++] = (DEC(bufin[j+1]) << 4) | (DEC(bufin[j+2]) >> 2);
-		if('=' != bufin[j+3]) bufout[i++] = (DEC(bufin[j+2]) << 6) | DEC(bufin[j+3]);
+		p[i++] = (s_base64_dec[source[0]] << 2) | (s_base64_dec[source[1]] >> 4);
+		if ('=' != source[2]) p[i++] = (s_base64_dec[source[1]] << 4) | (s_base64_dec[source[2]] >> 2);
+		if ('=' != source[3]) p[i++] = (s_base64_dec[source[2]] << 6) | s_base64_dec[source[3]];
+	}
+	return i;
+}
+
+static const char* s_base16_enc = "0123456789ABCDEF";
+static const uint8_t s_base16_dec[128] = {
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 0, 0, 0, 0, 0, /* 0 - 9 */
+	0,10,11,12,13,14,15, 0, 0, 0, 0, 0, 0, 0, 0, 0, /* A - F */
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	0,10,11,12,13,14,15, 0, 0, 0, 0, 0, 0, 0, 0, 0, /* a - f */
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+};
+
+// RFC3548 6. Base 16 Encoding (p8)
+size_t base16_encode(char* target, const void *source, size_t bytes)
+{
+	size_t i;
+	const uint8_t* p;
+	p = (const uint8_t*)source;
+	for (i = 0; i < bytes; i++)
+	{
+		target[i * 2] = s_base16_enc[(*p >> 4) & 0x0F];
+		target[i * 2 + 1] = s_base16_enc[*p & 0x0F];
+		++p;
+	}
+	return bytes * 2;
+}
+
+size_t base16_decode(void* target, const char *source, size_t bytes)
+{
+	size_t i;
+	uint8_t* p;
+	p = (uint8_t*)target;
+	assert(0 == bytes % 2);
+	for (i = 0; i < bytes / 2; i++)
+	{
+		p[i] = s_base16_dec[source[i * 2] & 0x7F] << 4;
+		p[i] |= s_base16_dec[source[i * 2 + 1] & 0x7F];
 	}
 	return i;
 }
@@ -127,16 +150,19 @@ void base64_test(void)
 	char source[512];
 	char target[512];
 
-	assert(8 == base64_encode(source, "4444", 4)); // NDQ0NA==
+	assert(8 == base64_encode(source, "4444", 4));
 	assert(4 == base64_decode(target, source, 8));
+	assert(0 == memcmp(source, "NDQ0NA==", 8));
 	assert(0 == memcmp(target, "4444", 4));
 
-	assert(8 == base64_encode(source, "55555", 5)); // NTU1NTU=
+	assert(8 == base64_encode(source, "55555", 5));
 	assert(5 == base64_decode(target, source, 8));
+	assert(0 == memcmp(source, "NTU1NTU=", 8));
 	assert(0 == memcmp(target, "55555", 5));
 
-	assert(8 == base64_encode(source, "666666", 6)); // NjY2NjY2
+	assert(8 == base64_encode(source, "666666", 6));
 	assert(6 == base64_decode(target, source, 8));
+	assert(0 == memcmp(source, "NjY2NjY2", 8));
 	assert(0 == memcmp(target, "666666", 6));
 
 	// RFC2617 2.Basic Authentication Scheme (p6)
@@ -146,5 +172,9 @@ void base64_test(void)
 	assert(strlen(s) == base64_decode(target, source, strlen(r)));
 	assert(0 == memcmp(source, r, strlen(r)));
 	assert(0 == memcmp(target, s, strlen(s)));
+
+	assert(8 == base16_encode(source, "4444", 4));
+	assert(4 == base16_decode(target, source, 8));
+	assert(0 == memcmp(target, "4444", 4));
 }
 #endif
