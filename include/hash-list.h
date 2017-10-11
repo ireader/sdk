556bdc7b41361a810fd9b99bdd9826c3e84dd917
ireader <tao3@outlook.com>
2017-10-11 14:40:20 +0800

    add hash list

---
+++ include/hash-list.h
@@ -0,0 +1,45 @@
+#ifndef _hash_list_h_
+#define _hash_list_h_
+
+struct hash_node_t
+{
+	struct hash_node_t* next;
+	struct hash_node_t** pnext; // &prev->next
+};
+
+struct hash_head_t
+{
+	struct hash_node_t *first;
+};
+
+static inline int hash_list_empty(struct hash_head_t* head)
+{
+	return !head->first;
+}
+
+static inline void hash_list_add(struct hash_head_t* head, struct hash_node_t* node)
+{
+	if (head->first)
+		head->first->pnext = &node->next;
+	node->next = head->first;
+	node->pnext = &head->first;
+	head->first = node;
+}
+
+static inline void hash_list_del(struct hash_node_t* node)
+{
+	*node->pnext = node->next;
+	if (node->next)
+		node->next->pnext = node->pnext;
+}
+
+#define hash_list_entry(ptr, type, member)	\
+	((type*)( (char*)ptr - &(((type*)0)->member) ))
+
+#define hash_list_for_each(pos, head) \
+	for(pos = (head)->first; pos; pos = pos->next)
+
+#define list_for_each_safe(pos, n, head) \
+	for(pos = (head)->first; pos && (n = pos->next, 1); pos = n)
+
+#endif /* !_hash_table_h_ */
