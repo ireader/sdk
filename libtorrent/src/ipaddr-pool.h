7d684c19527166b902db554043a84bb2880f14eb
ireader <tao3@outlook.com>
2017-10-17 20:31:51 +0800

    add peer address pool

---
+++ libtorrent/src/ipaddr-pool.h
@@ -0,0 +1,19 @@
+#ifndef _peer_pool_h_
+#define _peer_pool_h_
+
+#include "sys/sock.h"
+
+typedef struct ipaddr_pool_t ipaddr_pool_t;
+
+ipaddr_pool_t* ipaddr_pool_create();
+void ipaddr_pool_destroy(ipaddr_pool_t* pool);
+void ipaddr_pool_clear(ipaddr_pool_t* pool);
+
+int ipaddr_pool_empty(ipaddr_pool_t* pool);
+size_t ipaddr_pool_size(ipaddr_pool_t* pool);
+
+/// @return 0-ok, other-error
+int ipaddr_pool_push(ipaddr_pool_t* pool, const struct sockaddr_storage* addr);
+int ipaddr_pool_pop(ipaddr_pool_t* pool, struct sockaddr_storage* addr);
+
+#endif /* !_peer_pool_h_ */
