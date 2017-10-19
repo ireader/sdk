510a65a5d7d84942b4f6c58c6a6f29eac264972e
ireader <tao3@outlook.com>
2017-10-19 15:54:50 +0800

    -

--- libtorrent/src/metainfo.c
+++ libtorrent/src/metainfo.c
@@ -7,7 +7,7 @@
 
 int hash_sha1(const uint8_t* data, size_t bytes, uint8_t sha1[20]);
 
-static int metainfo_read_trackers(const struct bvalue_t* anounces, struct metainfo_t* tor)
+static int metainfo_read_trackers(const struct bvalue_t* anounces, struct metainfo_t* meta)
 {
 	void* p;
 	size_t i;
@@ -18,25 +18,26 @@ static int metainfo_read_trackers(const struct bvalue_t* anounces, struct metain
 		return -1;
 	}
 
-	assert(1 == tor->tracker_count);
-	p = realloc(tor->trackers, sizeof(char*) * (1 + anounces->v.list.count));
+	assert(1 == meta->tracker_count);
+	p = realloc(meta->trackers, sizeof(char*) * (1 + anounces->v.list.count));
 	if (!p)
 		return -1;
 
-	tor->trackers = (char**)p;
+	meta->trackers = (char**)p;
 	for (i = 0; i < anounces->v.list.count; i++)
 	{
-		if (0 != bencode_get_string_ex(anounces->v.list.values + i, &tor->trackers[tor->tracker_count++]))
+		if (0 != bencode_get_string_ex(anounces->v.list.values + i, &meta->trackers[meta->tracker_count++]))
 			return -1;
 	}
 
 	return 0;
 }
 
-static int metainfo_read_files(const struct bvalue_t* files, struct metainfo_t* tor)
+static int metainfo_read_files(const struct bvalue_t* files, struct metainfo_t* meta)
 {
 	int r;
 	size_t i, j;
+	int64_t offset = 0;
 
 	if (BT_LIST != files->type)
 	{
@@ -44,9 +45,9 @@ static int metainfo_read_files(const struct bvalue_t* files, struct metainfo_t* 
 		return -1;
 	}
 
-	assert(0 == tor->file_count);
-	tor->files = malloc(sizeof(tor->files[0]) * (1 + files->v.list.count));
-	if (!tor->files)
+	assert(0 == meta->file_count);
+	meta->files = malloc(sizeof(meta->files[0]) * (1 + files->v.list.count));
+	if (!meta->files)
 		return -1;
 
 	for (r = i = 0; i < files->v.list.count && 0 == r; i++)
@@ -62,19 +63,22 @@ static int metainfo_read_files(const struct bvalue_t* files, struct metainfo_t* 
 		for (j = 0; j < 2 && 0 == r; j++)
 		{
 			if (0 == strcmp("length", file->v.dict.names[j].name))
-				r = bencode_get_int64(file->v.dict.values + j, &tor->files[tor->file_count].bytes);
+				r = bencode_get_int64(file->v.dict.values + j, &meta->files[meta->file_count].bytes);
 			else if (0 == strcmp("path", file->v.dict.names[j].name))
-				r = bencode_get_string_ex(file->v.dict.values + j, &tor->files[tor->file_count].name);
+				r = bencode_get_string_ex(file->v.dict.values + j, &meta->files[meta->file_count].name);
 			else
 				r = -1;
 		}
-		++tor->file_count;
+
+		meta->files[meta->file_count].offset = offset;
+		offset += meta->files[meta->file_count].bytes;
+		++meta->file_count;
 	}
 
 	return r;
 }
 
-static int metainfo_read_pieces(const struct bvalue_t* pieces, struct metainfo_t* tor)
+static int metainfo_read_pieces(const struct bvalue_t* pieces, struct metainfo_t* meta)
 {
 	if (BT_STRING != pieces->type || 0 != pieces->v.str.bytes % 20 || 0 == pieces->v.str.bytes)
 	{
@@ -82,17 +86,17 @@ static int metainfo_read_pieces(const struct bvalue_t* pieces, struct metainfo_t
 		return -1;
 	}
 
-	assert(0 == tor->piece_count);
-	tor->pieces = malloc(pieces->v.str.bytes);
-	if (!tor->pieces)
+	assert(0 == meta->piece_count);
+	meta->pieces = malloc(pieces->v.str.bytes);
+	if (!meta->pieces)
 		return -1;
 
-	tor->piece_count = pieces->v.str.bytes / 20;
-	memcpy(tor->pieces, pieces->v.str.value, pieces->v.str.bytes);
+	meta->piece_count = pieces->v.str.bytes / 20;
+	memcpy(meta->pieces, pieces->v.str.value, pieces->v.str.bytes);
 	return 0;
 }
 
-static int metainfo_read_info(const struct bvalue_t* info, struct metainfo_t* tor)
+static int metainfo_read_info(const struct bvalue_t* info, struct metainfo_t* meta)
 {
 	int r;
 	size_t i;
@@ -108,11 +112,11 @@ static int metainfo_read_info(const struct bvalue_t* info, struct metainfo_t* to
 	{
 		if (0 == strcmp("files", info->v.dict.names[i].name))
 		{
-			r = metainfo_read_files(info->v.dict.values + i, tor);
+			r = metainfo_read_files(info->v.dict.values + i, meta);
 		}
 		else if (0 == strcmp("name", info->v.dict.names[i].name))
 		{
-			r = bencode_get_string(info->v.dict.values + i, &tor->file_path);
+			r = bencode_get_string(info->v.dict.values + i, &meta->file_path);
 		}
 		else if (0 == strcmp("length", info->v.dict.names[i].name))
 		{
@@ -120,11 +124,11 @@ static int metainfo_read_info(const struct bvalue_t* info, struct metainfo_t* to
 		}
 		else if (0 == strcmp("pieces", info->v.dict.names[i].name))
 		{
-			r = metainfo_read_pieces(info->v.dict.values + i, tor);
+			r = metainfo_read_pieces(info->v.dict.values + i, meta);
 		}
 		else if (0 == strcmp("piece length", info->v.dict.names[i].name))
 		{
-			r = bencode_get_int64(info->v.dict.values + i, &tor->piece_bytes);
+			r = bencode_get_int64(info->v.dict.values + i, &meta->piece_bytes);
 		}
 		else
 		{
@@ -133,20 +137,21 @@ static int metainfo_read_info(const struct bvalue_t* info, struct metainfo_t* to
 	}
 
 	// single file mode
-	if (len > 0 && 0 == tor->file_count)
+	if (len > 0 && 0 == meta->file_count)
 	{
-		tor->files = malloc(sizeof(tor->files[0]));
-		if (!tor->files)
+		meta->files = malloc(sizeof(meta->files[0]));
+		if (!meta->files)
 			return -1;
-		tor->files[0].name = strdup("");
-		tor->files[0].bytes = len;
-		tor->file_count = 1;
+		meta->files[0].name = strdup("");
+		meta->files[0].bytes = len;
+		meta->files[0].offset = 0;
+		meta->file_count = 1;
 	}
 
 	return 0;
 }
 
-int metainfo_read(const uint8_t* ptr, size_t bytes, struct metainfo_t* tor)
+int metainfo_read(const uint8_t* ptr, size_t bytes, struct metainfo_t* meta)
 {
 	int r;
 	size_t i;
@@ -155,36 +160,36 @@ int metainfo_read(const uint8_t* ptr, size_t bytes, struct metainfo_t* tor)
 	if (BT_DICT != root.type)
 		return -1;
 
-	memset(tor, 0, sizeof(*tor));
-	tor->trackers = malloc(sizeof(char*));
-	if (!tor->trackers)
+	memset(meta, 0, sizeof(*meta));
+	meta->trackers = malloc(sizeof(char*));
+	if (!meta->trackers)
 		return -1;
 
 	for (r = i = 0; i < root.v.dict.count && 0 == r; i++)
 	{
 		if (0 == strcmp("announce", root.v.dict.names[i].name))
 		{
-			r = bencode_get_string(root.v.dict.values + i, &tor->trackers[tor->tracker_count++]);
+			r = bencode_get_string(root.v.dict.values + i, &meta->trackers[meta->tracker_count++]);
 		}
 		else if (0 == strcmp("announce-list", root.v.dict.names[i].name))
 		{
-			r = metainfo_read_trackers(root.v.dict.values + i, tor);
+			r = metainfo_read_trackers(root.v.dict.values + i, meta);
 		}
 		else if (0 == strcmp("comment", root.v.dict.names[i].name))
 		{
-			r = bencode_get_string(root.v.dict.values + i, &tor->comment);
+			r = bencode_get_string(root.v.dict.values + i, &meta->comment);
 		}
 		else if (0 == strcmp("created by", root.v.dict.names[i].name))
 		{
-			r = bencode_get_string(root.v.dict.values + i, &tor->author);
+			r = bencode_get_string(root.v.dict.values + i, &meta->author);
 		}
 		else if (0 == strcmp("creation date", root.v.dict.names[i].name))
 		{
-			r = bencode_get_int64(root.v.dict.values + i, &tor->create);
+			r = bencode_get_int64(root.v.dict.values + i, &meta->create);
 		}
 		else if (0 == strcmp("info", root.v.dict.names[i].name))
 		{
-			r = metainfo_read_info(root.v.dict.values + i, tor);
+			r = metainfo_read_info(root.v.dict.values + i, meta);
 		}
 		else
 		{
@@ -196,143 +201,143 @@ int metainfo_read(const uint8_t* ptr, size_t bytes, struct metainfo_t* tor)
 	return r;
 }
 
-static uint8_t* metainfo_write_info(uint8_t* ptr, const uint8_t* end, const struct metainfo_t* tor)
+static uint8_t* metainfo_write_info(uint8_t* ptr, const uint8_t* end, const struct metainfo_t* meta)
 {
 	size_t i;
 
 	if (ptr < end) *ptr++ = 'd';
-	if (1 == tor->file_count)
+	if (1 == meta->file_count)
 	{
 		ptr = bencode_write_string(ptr, end, "name", 4);
-		ptr = bencode_write_string(ptr, end, tor->files[0].name, strlen(tor->files[0].name));
+		ptr = bencode_write_string(ptr, end, meta->files[0].name, strlen(meta->files[0].name));
 		ptr = bencode_write_string(ptr, end, "length", 6);
-		ptr = bencode_write_int(ptr, end, tor->files[0].bytes);
+		ptr = bencode_write_int(ptr, end, meta->files[0].bytes);
 	}
 	else
 	{
 		ptr = bencode_write_string(ptr, end, "files", 5);
 		if (ptr < end) *ptr++ = 'l';
-		for (i = 0; i < tor->file_count; i++)
+		for (i = 0; i < meta->file_count; i++)
 		{
 			if (ptr < end) *ptr++ = 'd';
 			ptr = bencode_write_string(ptr, end, "length", 6);
-			ptr = bencode_write_int(ptr, end, tor->files[i].bytes);
+			ptr = bencode_write_int(ptr, end, meta->files[i].bytes);
 			ptr = bencode_write_string(ptr, end, "path", 4);
 			if (ptr < end) *ptr++ = 'l';
-			ptr = bencode_write_string(ptr, end, tor->files[i].name, strlen(tor->files[i].name));
+			ptr = bencode_write_string(ptr, end, meta->files[i].name, strlen(meta->files[i].name));
 			if (ptr < end) *ptr++ = 'e';
 			if (ptr < end) *ptr++ = 'e';
 		}
 		if (ptr < end) *ptr++ = 'e';
 
 		ptr = bencode_write_string(ptr, end, "name", 4);
-		ptr = bencode_write_string(ptr, end, tor->file_path, strlen(tor->file_path));
+		ptr = bencode_write_string(ptr, end, meta->file_path, strlen(meta->file_path));
 	}
 
 	ptr = bencode_write_string(ptr, end, "piece length", 12);
-	ptr = bencode_write_int(ptr, end, tor->piece_bytes);
+	ptr = bencode_write_int(ptr, end, meta->piece_bytes);
 
 	ptr = bencode_write_string(ptr, end, "pieces", 6);
-	ptr = bencode_write_string(ptr, end, (const char*)tor->pieces, tor->piece_count * 20);
+	ptr = bencode_write_string(ptr, end, (const char*)meta->pieces, meta->piece_count * 20);
 	if (ptr < end) *ptr++ = 'e';
 
 	return ptr;
 }
 
-int metainfo_write(const struct metainfo_t* tor, uint8_t* buffer, size_t bytes)
+int metainfo_write(const struct metainfo_t* meta, uint8_t* buffer, size_t bytes)
 {
 	size_t i;
 	uint8_t *ptr, *end;
 	ptr = buffer;
 	end = ptr + bytes;
 
-	if (tor->tracker_count < 0 || tor->file_count < 1)
+	if (meta->tracker_count < 0 || meta->file_count < 1)
 		return -1;
 
 	if (ptr < end) *ptr++ = 'd';
 
 	// announce
 	ptr = bencode_write_string(ptr, end, "announce", 8);
-	ptr = bencode_write_string(ptr, end, tor->trackers[0], strlen(tor->trackers[0]));
+	ptr = bencode_write_string(ptr, end, meta->trackers[0], strlen(meta->trackers[0]));
 
 	// announce-list
-	if (tor->tracker_count > 1)
+	if (meta->tracker_count > 1)
 	{
 		ptr = bencode_write_string(ptr, end, "announce-list", 13);
 		if (ptr < end) *ptr++ = 'l';
-		for (i = 1; i < tor->tracker_count; i++)
+		for (i = 1; i < meta->tracker_count; i++)
 		{
 			if (ptr < end) *ptr++ = 'l';
-			ptr = bencode_write_string(ptr, end, tor->trackers[i], strlen(tor->trackers[i]));
+			ptr = bencode_write_string(ptr, end, meta->trackers[i], strlen(meta->trackers[i]));
 			if (ptr < end) *ptr++ = 'e';
 		}
 		if (ptr < end) *ptr++ = 'e';
 	}
 
 	// comment
-	if (tor->comment)
+	if (meta->comment)
 	{
 		ptr = bencode_write_string(ptr, end, "comment", 7);
-		ptr = bencode_write_string(ptr, end, tor->comment, strlen(tor->comment));
+		ptr = bencode_write_string(ptr, end, meta->comment, strlen(meta->comment));
 	}
 
 	// created by
-	if (tor->author)
+	if (meta->author)
 	{
 		ptr = bencode_write_string(ptr, end, "created by", 10);
-		ptr = bencode_write_string(ptr, end, tor->author, strlen(tor->author));
+		ptr = bencode_write_string(ptr, end, meta->author, strlen(meta->author));
 	}
 
 	// creation date
-	if (tor->create)
+	if (meta->create)
 	{
 		ptr = bencode_write_string(ptr, end, "creation date", 13);
-		ptr = bencode_write_int(ptr, end, tor->create);
+		ptr = bencode_write_int(ptr, end, meta->create);
 	}
 
 	// info
 	ptr = bencode_write_string(ptr, end, "info", 4);
-	ptr = metainfo_write_info(ptr, end, tor);
+	ptr = metainfo_write_info(ptr, end, meta);
 
 	if (ptr < end) *ptr++ = 'e';
 	return ptr - buffer;
 }
 
-int metainfo_free(struct metainfo_t* tor)
+int metainfo_free(struct metainfo_t* meta)
 {
 	size_t i;
 
-	if (tor->trackers)
+	if (meta->trackers)
 	{
-		for (i = 0; i < tor->tracker_count; i++)
-			free(tor->trackers[i]);
-		free(tor->trackers);
+		for (i = 0; i < meta->tracker_count; i++)
+			free(meta->trackers[i]);
+		free(meta->trackers);
 	}
 
-	for (i = 0; i < tor->file_count; i++)
-		free(tor->files[i].name);
-	free(tor->files);
+	for (i = 0; i < meta->file_count; i++)
+		free(meta->files[i].name);
+	free(meta->files);
 
-	if (tor->comment)
-		free(tor->comment);
-	if (tor->author)
-		free(tor->author);
-	if (tor->pieces)
-		free(tor->pieces);
-	if (tor->file_path)
-		free(tor->file_path);
+	if (meta->comment)
+		free(meta->comment);
+	if (meta->author)
+		free(meta->author);
+	if (meta->pieces)
+		free(meta->pieces);
+	if (meta->file_path)
+		free(meta->file_path);
 	return 0;
 }
 
-int metainfo_hash(const struct metainfo_t* tor, uint8_t sha1[20])
+int metainfo_hash(const struct metainfo_t* meta, uint8_t sha1[20])
 {
 	uint8_t *info, *ptr;
 
-	if (tor->file_count < 1)
+	if (meta->file_count < 1)
 		return -1;
 
 	info = (uint8_t*)malloc(2 * 1024 * 1024);
-	ptr = metainfo_write_info(info, info + 2 * 1024 * 1024, tor);
+	ptr = metainfo_write_info(info, info + 2 * 1024 * 1024, meta);
 	if (ptr + 1 < info + 2 * 1024 * 1024)
 	{
 		hash_sha1(info, ptr - info, sha1);
