adcf601316fa464569603afbe2e2d6d6f1a7fd72
ireader <tao3@outlook.com>
2017-10-22 15:43:17 +0800

    enable rtsp_header_range_test

--- test/rtsp-test.c
+++ test/rtsp-test.c
@@ -7,7 +7,7 @@ void rtsp_header_transport_test(void);
 
 void rtsp_test(void)
 {
-	//	rtsp_header_range_test();
+	rtsp_header_range_test();
 	rtsp_header_session_test();
 	rtsp_header_rtp_info_test();
 	rtsp_header_transport_test();
