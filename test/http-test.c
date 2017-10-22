0fd3a865abe9a46d5aeac723ff5276cd5621e4c1
ireader <tao3@outlook.com>
2017-10-22 15:43:09 +0800

    add http_client_test

--- test/http-test.c
+++ test/http-test.c
@@ -5,6 +5,8 @@ void http_request_test(void);
 void http_header_host_test(void);
 void http_header_content_type_test(void);
 void http_header_range_test(void);
+void http_client_test(void);
+void http_client_test2(void);
 
 void http_test(void)
 {
@@ -13,4 +15,7 @@ void http_test(void)
 	http_header_host_test();
 	http_header_content_type_test();
 	http_header_range_test();
+
+	http_client_test();
+	http_client_test2();
 }
