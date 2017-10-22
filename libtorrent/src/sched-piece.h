81822d0f8182dc3142fadb8d4acde0b0f38a2878
ireader <tao3@outlook.com>
2017-10-22 15:22:13 +0800

    CPiece::Add split -> Add + GetRegin

--- libtorrent/src/sched-piece.h
+++ libtorrent/src/sched-piece.h
@@ -10,12 +10,16 @@
 #include <vector>
 #include <assert.h>
 
+#define DIV_ROUND_UP(v, n) ( ((v) + (n) - 1) / (n) )
+#define PIECE_BYTES_TO_SLICES(bytes) DIV_ROUND_UP(bytes, N_PIECE_SLICE)
+
 namespace peer
 {
 	enum { INIT = 0, IDLE, WORKING };
-	enum { BITFILED = 4 };
 	struct CPeer
 	{
+		enum { BITFILED = 32 };
+
 		struct peer_t* m_peer;
 		struct torrent_sched_t* m_disp;
 		struct sockaddr_storage addr;
@@ -30,7 +34,7 @@ namespace peer
 		uint32_t piece;
 		uint32_t begin;
 		uint32_t bytes;
-		uint8_t bitfield[BITFILED];
+		uint8_t bitfield[DIV_ROUND_UP(BITFILED, 8)];
 
 		bool choke;
 		const uint8_t* bitmap;
@@ -48,15 +52,15 @@ namespace peer
 	class CPiece
 	{
 		mutable ThreadLocker m_locker;
+		mutable uint8_t* m_bitfield; // task assign bitmap
 		std::vector<std::shared_ptr<CPeer> > m_peers;
 		piece_t* m_piece;
-		uint8_t* m_bitfield; // task assign bitmap
 
 	public:
 		CPiece(uint32_t piece, uint32_t bytes, const uint8_t sha1[20])
 		{
 			m_piece = piece_create(piece, bytes, sha1);
-			m_bitfield = new uint8_t[((bytes + N_PIECE_SLICE - 1)/N_PIECE_SLICE + 7)/8];
+			m_bitfield = new uint8_t[DIV_ROUND_UP(PIECE_BYTES_TO_SLICES(bytes), 8)];
 		}
 
 		~CPiece()
@@ -87,35 +91,18 @@ namespace peer
 			m_peers.clear();
 		}
 
-		bool Add(std::shared_ptr<CPeer>& peer)
+		bool Add(std::shared_ptr<CPeer>& peer, uint32_t begin, uint32_t length)
 		{
+			assert(begin + length <= PIECE_BYTES_TO_SLICES(m_piece->bytes));
 			peer->status = peer::WORKING;
 			peer->clock = system_clock();
 			peer->total = 0;
 			peer->piece = m_piece->piece;
-			peer->begin = 0;
-			peer->bytes = 0;
+			peer->begin = begin * N_PIECE_SLICE;
+			peer->bytes = length * N_PIECE_SLICE;
 			memset(peer->bitfield, 0, sizeof(peer->bitfield));
 
-			unsigned int bits = (m_piece->bytes + N_PIECE_SLICE - 1) / N_PIECE_SLICE; 
 			AutoThreadLocker guard(m_locker);
-			memcpy(m_bitfield, m_piece->bitfield, (bits + 7) / 8);
-			for (auto it = m_peers.begin(); it != m_peers.end(); ++it)
-			{
-				bitmap_set(m_bitfield, (*it)->begin / N_PIECE_SLICE, (*it)->bytes / N_PIECE_SLICE);
-			}
-
-			unsigned int begin = bitmap_find_first_zero(m_bitfield, bits);
-			if (begin >= bits)
-			{
-				assert(0);
-				peer->status = peer::IDLE;
-				return false;
-			}
-
-			unsigned int length = bitmap_count_next_zero(m_bitfield, bits, begin);
-			peer->begin = begin * N_PIECE_SLICE;
-			peer->bytes = (sizeof(m_piece->bitfield) < length ? length : sizeof(m_piece->bitfield)) * 8 * N_PIECE_SLICE;
 			m_peers.push_back(peer);
 			return true;
 		}
@@ -141,6 +128,28 @@ namespace peer
 		{
 			AutoThreadLocker guard(m_locker);
 			return m_peers.size();
+		}
+
+		bool GetRegin(uint32_t& begin, uint32_t& length) const
+		{
+			unsigned int bits = PIECE_BYTES_TO_SLICES(m_piece->bytes);
+			AutoThreadLocker guard(m_locker);
+			memcpy(m_bitfield, m_piece->bitfield, DIV_ROUND_UP(bits, 8));
+			for (auto it = m_peers.begin(); it != m_peers.end(); ++it)
+			{
+				bitmap_set(m_bitfield, (*it)->begin / N_PIECE_SLICE, (*it)->bytes / N_PIECE_SLICE);
+			}
+
+			begin = bitmap_find_first_zero(m_bitfield, bits);
+			if (begin >= bits)
+			{
+				assert(bits = bitmap_weight(m_bitfield, bits));
+				return false;
+			}
+
+			length = bitmap_count_next_zero(m_bitfield, bits, begin);
+			length = CPeer::BITFILED > length ? length : CPeer::BITFILED;
+			return true;
 		}
 	};
 }
