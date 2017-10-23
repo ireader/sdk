dda92135eb243b0bd4b56254983f00500462b10b
ireader <tao3@outlook.com>
2017-10-23 09:48:53 +0800

    add strrev

---
+++ source/string/strrev.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <string.h>
+
+char *strrev(char *str)
+{
+	char *p1, *p2;
+
+	if (!str || !*str)
+		return str;
+
+	for (p1 = str, p2 = str + strlen(str) - 1; p2 > p1; ++p1, --p2)
+	{
+		*p1 ^= *p2;
+		*p2 ^= *p1;
+		*p1 ^= *p2;
+	}
+
+	return str;
+}
