921a815a7afe381397abc815cb288ffc417e8c6b
ireader <tao3@outlook.com>
2017-12-01 11:35:06 +0800

    aio-worker

--- libhttp/test/http-server-test.cpp
+++ libhttp/test/http-server-test.cpp
@@ -1,5 +1,5 @@
 #include "aio-socket.h"
-#include "aio-timeout.h"
+#include "aio-worker.h"
 #include "http-server.h"
 #include "http-route.h"
 #include "sys/thread.h"
@@ -32,42 +32,22 @@ static int http_server_ondownload(void* /*http*/, http_session_t* session, const
 	return http_server_send(session, 404, "", 0, NULL, NULL);
 }
 
-static int STDCALL http_server_worker(void* param)
-{
-	int idx = (int)(intptr_t)param;
-	while (aio_socket_process(200) >= 0 || errno == EINTR) // ignore epoll EINTR
-	{
-	}
-
-	printf("%s[%d] exit => %d.\n", __FUNCTION__, idx, errno);
-	return 0;
-}
-
 extern "C" void http_server_test(const char* ip, int port)
 {
 	size_t num = 0;
 
-//	num = system_getcpucount() * 2;
-	aio_socket_init(num + 1);
+	num = system_getcpucount() * 2;
+	aio_worker_init(num + 1);
 
 	http_server_t* http = http_server_create(ip, port);
 	http_server_set_handler(http, http_server_route, http);
 	http_server_addroute("/download/", http_server_ondownload);
 
-	// aio worker
-	while (num-- > 0)
-	{
-		pthread_t thread;
-		thread_create(&thread, http_server_worker, (void*)(intptr_t)num);
-		thread_detach(thread);
-	}
-
 	// timeout process
 	while (aio_socket_process(10000) >= 0)
 	{
-		aio_timeout_process();
 	}
 
 	http_server_destroy(http);
-	aio_socket_clean();
+	aio_worker_clean(num + 1);
 }
