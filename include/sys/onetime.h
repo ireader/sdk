749246fd6449ae64ca93c1f2813048db8086d753
ireader <tao3@outlook.com>
2018-03-30 17:00:03 +0800

    add XP support

--- include/sys/onetime.h
+++ include/sys/onetime.h
@@ -8,8 +8,13 @@
 
 #if defined(OS_WINDOWS)
 #include <Windows.h>
+#if(_WIN32_WINNT >= 0x0600) // vista
 typedef INIT_ONCE			onetime_t;
 #define ONETIME_INIT		INIT_ONCE_STATIC_INIT
+#else
+typedef LONG			    onetime_t;
+#define ONETIME_INIT		0
+#endif
 #else
 #include <pthread.h>
 typedef pthread_once_t		onetime_t;
@@ -31,8 +36,33 @@ static BOOL CALLBACK onetime_callback(PINIT_ONCE once, PVOID param, PVOID* conte
 static inline int onetime_exec(onetime_t* key, onetime_routine routine)
 {
 #if defined(OS_WINDOWS)
+#if(_WIN32_WINNT < 0x0600) // vista
+    for (;;)
+    {
+        switch (InterlockedCompareExchange(key, 2, 0))
+        {
+        case 0: // init
+            routine();
+            InterlockedExchange(key, 1);
+            return 0;
+
+        case 1: // finished
+            return 0;
+
+        case 2: // running
+            SwitchToThread(); // sleep(0)
+            break;
+
+        default:
+            assert(0);
+            return -1;
+        }
+    }
+    return 0;
+#else
 	// http://stackoverflow.com/questions/12358843/why-are-function-pointers-and-data-pointers-incompatible-in-c-c
 	return InitOnceExecuteOnce(key, onetime_callback, &routine, NULL) ? 0 : GetLastError();
+#endif
 #else
 	return pthread_once(key, routine);
 #endif
