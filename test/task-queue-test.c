9da6ec8974d810a498f57363968ebbe18b750fa7
ireader <tao3@outlook.com>
2018-04-03 12:50:12 +0800

    sema.h

--- test/task-queue-test.c
+++ test/task-queue-test.c
@@ -1,17 +1,17 @@
 #include "cstringext.h"
 #include "sys/thread.h"
-#include "sys/semaphore.h"
+#include "sys/sema.h"
 #include "task-queue.h"
 #include "thread-pool.h"
 #include <stdio.h>
 
 #define N_TASK 1000
 
-static semaphore_t s_sema;
+static sema_t s_sema;
 
 static void tasktest(void* param)
 {
-	semaphore_post(&s_sema);
+	sema_post(&s_sema);
 }
 
 void task_queue_test(void)
@@ -20,7 +20,7 @@ void task_queue_test(void)
 	thread_pool_t pool;
 	task_queue_t taskQ;
 	
-	semaphore_create(&s_sema, NULL, 0);
+	sema_create(&s_sema, NULL, 0);
 	pool = thread_pool_create(8, 8, 8);
 	taskQ = task_queue_create(pool, 8);
 	for (i=0; i < N_TASK; ++i)
@@ -29,11 +29,11 @@ void task_queue_test(void)
 	}
 
 	for(i = 0; i < N_TASK; ++i)
-		semaphore_wait(&s_sema);
+		sema_wait(&s_sema);
 
 	task_queue_destroy(taskQ);
 	thread_pool_destroy(pool);
-	semaphore_destroy(&s_sema);
+	sema_destroy(&s_sema);
 
 	printf("task-queue test ok\n");
 }
