4cb1efe721f1dc969e63dbb4b5a47c960df6fb7b
ireader <tao3@outlook.com>
2018-04-20 23:15:24 +0800

    aio-tcp-transport life reference count

--- libaio/src/aio-tcp-transport.c
+++ libaio/src/aio-tcp-transport.c
@@ -3,6 +3,7 @@
 #include "aio-send.h"
 #include "aio-rwutil.h"
 #include "sys/atomic.h"
+#include "sys/system.h"
 #include "sys/spinlock.h"
 #include <stdlib.h>
 #include <string.h>
@@ -13,11 +14,13 @@
 
 struct aio_tcp_transport_t
 {
-	spinlock_t locker;
+	int32_t		 ref;
+	spinlock_t   locker;
 	aio_socket_t socket;
 	
-	int rtimeout;
-	int wtimeout;
+	int rtimeout; // recv timeout
+	int wtimeout; // send timeout
+	uint64_t wclock; // last sent data clock, for check connection alive
 	struct aio_recv_t recv;
 	struct aio_socket_rw_t send;
 
@@ -25,9 +28,13 @@ struct aio_tcp_transport_t
 	void* param;
 };
 
-static void aio_tcp_transport_ondestroy(void* param);
-static void aio_tcp_transport_onrecv(void* param, int code, size_t bytes);
-static void aio_tcp_transport_onsend(void* param, int code, size_t bytes);
+#define AIO_TRANSPORT_ADDREF(t)		atomic_increment32(&t->ref)
+#define AIO_TRANSPORT_ONFAIL(t, r)	if(0 != r) aio_tcp_transport_release(t)
+
+static void aio_socket_onclose(void* param);
+static void aio_socket_onrecv(void* param, int code, size_t bytes);
+static void aio_socket_onsend(void* param, int code, size_t bytes);
+static void aio_tcp_transport_release(struct aio_tcp_transport_t*);
 
 struct aio_tcp_transport_t* aio_tcp_transport_create(socket_t socket, struct aio_tcp_transport_handler_t *handler, void* param)
 {
@@ -41,9 +48,10 @@ struct aio_tcp_transport_t* aio_tcp_transport_create(socket_t socket, struct aio
 struct aio_tcp_transport_t* aio_tcp_transport_create2(aio_socket_t aio, struct aio_tcp_transport_handler_t *handler, void* param)
 {
 	struct aio_tcp_transport_t* t;
-	t = (struct aio_tcp_transport_t*)malloc(sizeof(*t));
+	t = (struct aio_tcp_transport_t*)calloc(1, sizeof(*t));
 	if (!t) return NULL;
 
+	t->ref = 1;
 	t->socket = aio;
 	t->param = param;
 	t->rtimeout = TIMEOUT_RECV;
@@ -63,46 +71,70 @@ int aio_tcp_transport_destroy(struct aio_tcp_transport_t* t)
 
 	if (invalid_aio_socket == socket)
 		return 0;
-	return aio_socket_destroy(socket, aio_tcp_transport_ondestroy, t);
+	return aio_socket_destroy(socket, aio_socket_onclose, t);
 }
 
+static void aio_tcp_transport_release(struct aio_tcp_transport_t* t)
+{
+	if (0 == atomic_decrement32(&t->ref))
+	{
+		assert(invalid_aio_socket == t->socket);
+		if (t->handler.ondestroy)
+			t->handler.ondestroy(t->param);
+
+		spinlock_destroy(&t->locker);
+#if defined(DEBUG) || defined(_DEBUG)
+		memset(t, 0xCC, sizeof(*t));
+#endif
+		free(t);
+	}
+}
 int aio_tcp_transport_send(struct aio_tcp_transport_t* t, const void* data, size_t bytes)
 {
 	int r = -1;
+	AIO_TRANSPORT_ADDREF(t);
 	spinlock_lock(&t->locker);
 	if (invalid_aio_socket != t->socket)
-		r = aio_socket_send_all(&t->send, t->wtimeout, t->socket, data, bytes, aio_tcp_transport_onsend, t);
+		r = aio_socket_send_all(&t->send, t->wtimeout, t->socket, data, bytes, aio_socket_onsend, t);
 	spinlock_unlock(&t->locker);
+	AIO_TRANSPORT_ONFAIL(t, r);
 	return r;
 }
 
 int aio_tcp_transport_send_v(struct aio_tcp_transport_t* t, socket_bufvec_t *vec, int n)
 {
 	int r = -1;
+	AIO_TRANSPORT_ADDREF(t);
 	spinlock_lock(&t->locker);
 	if (invalid_aio_socket != t->socket)
-		r = aio_socket_send_v_all(&t->send, t->wtimeout, t->socket, vec, n, aio_tcp_transport_onsend, t);
+		r = aio_socket_send_v_all(&t->send, t->wtimeout, t->socket, vec, n, aio_socket_onsend, t);
 	spinlock_unlock(&t->locker);
+	AIO_TRANSPORT_ONFAIL(t, r);
 	return r;
 }
 
 int aio_tcp_transport_recv(struct aio_tcp_transport_t* t, void* data, size_t bytes)
 {
 	int r = -1;
+	AIO_TRANSPORT_ADDREF(t);
 	spinlock_lock(&t->locker);
 	if(invalid_aio_socket != t->socket)
-		r = aio_recv(&t->recv, t->rtimeout, t->socket, data, bytes, aio_tcp_transport_onrecv, t);
+		r = aio_recv(&t->recv, t->rtimeout, t->socket, data, bytes, aio_socket_onrecv, t);
 	spinlock_unlock(&t->locker);
+	AIO_TRANSPORT_ONFAIL(t, r);
 	return r;
 }
 
 int aio_tcp_transport_recv_v(struct aio_tcp_transport_t* t, socket_bufvec_t *vec, int n)
 {
 	int r = -1;
+
+	AIO_TRANSPORT_ADDREF(t);
 	spinlock_lock(&t->locker);
 	if (invalid_aio_socket != t->socket)
-		r = aio_recv_v(&t->recv, t->rtimeout, t->socket, vec, n, aio_tcp_transport_onrecv, t);
+		r = aio_recv_v(&t->recv, t->rtimeout, t->socket, vec, n, aio_socket_onrecv, t);
 	spinlock_unlock(&t->locker);
+	AIO_TRANSPORT_ONFAIL(t, r);
 	return r;
 }
 
@@ -122,33 +154,38 @@ void aio_tcp_transport_set_timeout(struct aio_tcp_transport_t* t, int recvMS, in
 	t->wtimeout = sendMS;
 }
 
-static void aio_tcp_transport_onrecv(void* param, int code, size_t bytes)
+static void aio_socket_onrecv(void* param, int code, size_t bytes)
 {
 	struct aio_tcp_transport_t* t;
 	t = (struct aio_tcp_transport_t*)param;
+
+	if (ETIMEDOUT == code 
+		&& t->wclock + t->rtimeout > system_clock() 
+		&& 0 == aio_recv_retry(&t->recv, t->rtimeout))
+	{
+		// if we have active send connection, recv timeout maybe normal case
+		// e.g. RTMP play session
+		return;
+	}
+
 	// enable bytes = 0 callback to notify socket close
 	t->handler.onrecv(t->param, code, bytes);
+	aio_tcp_transport_release(t);
 }
 
-static void aio_tcp_transport_onsend(void* param, int code, size_t bytes)
+static void aio_socket_onsend(void* param, int code, size_t bytes)
 {
 	struct aio_tcp_transport_t* t;
 	t = (struct aio_tcp_transport_t*)param;
+	t->wclock = system_clock();
 	t->handler.onsend(t->param, code, bytes);
+	aio_tcp_transport_release(t);
 }
 
-static void aio_tcp_transport_ondestroy(void* param)
+static void aio_socket_onclose(void* param)
 {
 	struct aio_tcp_transport_t* t;
 	t = (struct aio_tcp_transport_t*)param;
-	assert(invalid_aio_socket == t->socket);
-
-	if (t->handler.ondestroy)
-		t->handler.ondestroy(t->param);
-
-	spinlock_destroy(&t->locker);
-#if defined(DEBUG) || defined(_DEBUG)
-	memset(t, 0xCC, sizeof(*t));
-#endif
-	free(t);
+	aio_tcp_transport_release(t);
 }
+
