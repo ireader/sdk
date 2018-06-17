124e4c3ba9db07efb352431375374e2121e8b1da
ireader <tao3@outlook.com>
2018-06-17 22:29:27 +0800

    add cstrchr/cstrpbrk/cstrcpy/cstrncasecmp/cstrcasecmp

--- include/cstring.h
+++ include/cstring.h
@@ -2,12 +2,47 @@
 #define _cstring_h_
 
 #include <string.h>
+#include <stdlib.h>
 
 struct cstring_t
 {
 	const char* p;
 	size_t n;
 };
+
+static inline int cstrvalid(const struct cstring_t* s)
+{
+	return (s->p && s->n) > 0 ? 1 : 0;
+}
+
+// same as strchr
+static inline const char* cstrchr(const struct cstring_t* s, int c)
+{
+	const char* p;
+	p = strchr(s->p, c);
+	return p && p < s->p + s->n ? p : NULL;
+}
+
+// same as strpbrk
+static inline const char* cstrpbrk(const struct cstring_t* s, const char* delimiters)
+{
+	const char* p;
+	p = strpbrk(s->p, delimiters);
+	return p && p < s->p + s->n ? p : NULL;
+}
+
+// same as strlcpy
+static inline size_t cstrcpy(const struct cstring_t* s, char* dst, size_t size)
+{
+	size_t n;
+	n = s->n > size ? size : s->n;
+	memcpy(dst, s->p, n);
+
+	if(n < size)
+		dst[n] = 0; // fill with '\0'
+
+	return s->n;
+}
 
 // cstrltrim("  abc  ") => "abc  "
 static inline void cstrltrim(struct cstring_t* s, const char* trims)
@@ -51,19 +86,34 @@ static inline int cstrncmp(const struct cstring_t* s, const char* c, size_t n)
 
 	m = s->n > n ? n : s->n;
 	r = memcmp(s->p, c, m);
-	if (0 != r)
-		return r;
-	return s->n > m ? 1 : 0;
+	return 0 != r ? r : (s->n > n ? 1 : -1);
 }
 
 // cstrcmp("abc", "ab") => 1
 static inline int cstrcmp(const struct cstring_t* s, const char* c)
 {
+	return cstrncmp(s, c, strlen(c));
+}
+
+// same as strnicmp/strncasecmp
+static inline int cstrncasecmp(const struct cstring_t* s, const char* c, size_t n)
+{
 	int r;
-	size_t n;
-	n = strlen(c);
-	r = cstrncmp(s, c, n);
-	return 0 != r ? r : (s->n > n ? 1 : 0);
+	size_t m;
+
+	m = s->n > n ? n : s->n;
+#if defined(OS_WINDOWS)
+	r = strnicmp(s->p, c, m);
+#else
+	r = strcasecmp(s->p, c, m);
+#endif
+	return 0 != r ? r : (s->n > n ? 1 : -1);
+}
+
+// same as stricmp/strcasecmp
+static inline int cstrcasecmp(const struct cstring_t* s, const char* c)
+{
+	return cstrncasecmp(s, c, strlen(c));
 }
 
 // cstrprefix("abc", "ab") => 1
