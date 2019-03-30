2120cd23eaef6a91ee7454a61fc5fb278150fda5
ireader <ch.magus@163.com>
2019-03-30 21:05:06 +0800

    ice api

--- libice/include/ice-agent.h
+++ libice/include/ice-agent.h
@@ -9,9 +9,25 @@ struct ice_agent_t;
 struct ice_agent_t* ice_agent_create();
 int ice_agent_destroy(struct ice_agent_t* ice);
 
-int ice_add_local_candidate(struct ice_agent_t* ice, const struct ice_candidate_t* c);
-int ice_add_remote_candidate(struct ice_agent_t* ice, const struct ice_candidate_t* c);
+/// Add host candidate (with stun/turn server)
+/// @param[in] c host candidate (should set stun/turn server addr)
+int ice_add_local_candidate(struct ice_agent_t* ice, int stream, const struct ice_candidate_t* c);
 
+/// Add remote candidate(host/server reflexive/relayed)
+/// @param[in] c remote candidate
+int ice_add_remote_candidate(struct ice_agent_t* ice, int stream, const struct ice_candidate_t* c);
+
+/// Gather server reflexive and relayed candidates
+int ice_gather_stun_candidate(struct ice_agent_t* ice);
+
+/// Choosing default candidates
+int ice_get_default_candidate(struct ice_agent_t* ice, int stream, struct ice_candidate_t* c);
+
+typedef int(*ice_oncandidate)(void* param, int stream, const struct ice_candidate_t* c);
+int ice_list_local_candidate(struct ice_agent_t* ice, int stream, ice_oncandidate oncand, void* param);
+int ice_list_remote_candidate(struct ice_agent_t* ice, int stream, ice_oncandidate oncand, void* param);
+
+int ice_start(struct ice_agent_t* ice);
 
 #if defined(__cplusplus)
 }
