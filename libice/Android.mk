bf789550f15f80782281f1074c8a36f3a3b8ad30
ireader <tao3@outlook.com>
2019-09-28 15:32:20 +0800

    add android.mk

---
+++ libice/Android.mk
@@ -0,0 +1,15 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_CFLAGS += -DOS_LINUX -DOS_ANDROID
+LOCAL_LDLIBS +=
+
+LOCAL_C_INCLUDES := $(LOCAL_PATH)
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/include
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/../include
+
+LOCAL_SRC_FILES += $(wildcard src/*.c)
+LOCAL_SRC_FILES += $(wildcard src/*.cpp)
+
+LOCAL_MODULE := ice
+include $(BUILD_STATIC_LIBRARY)
