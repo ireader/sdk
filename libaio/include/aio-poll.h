a9bbba8b88390e5b8d1c87cf4b89ec543f48cba0
ireader <tao3@outlook.com>
2019-10-27 09:26:16 +0800

    add aio-poll (client use only)

---
+++ libaio/include/aio-poll.h
@@ -0,0 +1,39 @@
+#ifndef _aio_poll_h_
+#define _aio_poll_h_
+
+#include "sys/sock.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+struct aio_poll_t;
+
+enum
+{
+	AIO_POLL_IN = 0x01,
+	AIO_POLL_OUT = 0x02,
+};
+
+struct aio_poll_t* aio_poll_create();
+int aio_poll_destroy(struct aio_poll_t* poll);
+
+/// Read/Write callback
+/// @param[in] code 0-ok, ETIMEOUT-timeout, other-error
+/// @param[in] socket same as aio_poll_poll socket
+/// @param[in] flags AIO_POLL_IN/AIO_POLL_OUT
+/// @param[in] param same as aio_poll_poll param
+typedef void (*aio_poll_callback)(int code, socket_t socket, int flags, void* param);
+
+/// Async poll
+/// @param[in] flags AIO_POLL_IN/AIO_POLL_OUT
+/// @param[in] timeout poll timeout, in MS
+/// @param[in] callback poll callback
+/// @param[in] param user defined callback parameter
+/// @return 0-ok, other-error
+int aio_poll_poll(struct aio_poll_t* poll, socket_t socket, int flags, int timeout, aio_poll_callback callback, void* param);
+
+#ifdef __cplusplus
+}
+#endif
+#endif /* !_aio_poll_h_ */
---
+++ libaio/src/aio-poll.c
@@ -0,0 +1,243 @@
+#include "aio-poll.h"
+#include "sys/system.h"
+#include "sys/thread.h"
+#include "sys/locker.h"
+#include "sys/pollfd.h"
+#include "sockutil.h"
+#include "list.h"
+#include <errno.h>
+#include <stdlib.h>
+#include <assert.h>
+
+#define N_SOCKETS FD_SETSIZE
+
+struct aio_poll_socket_t
+{
+	struct list_head link;
+
+	socket_t fd;
+	int events;
+	int revents;
+	uint64_t expire;
+	aio_poll_callback callback;
+	void* param;
+};
+
+struct aio_poll_t
+{
+	locker_t locker;
+	struct list_head root;
+	struct list_head idles;
+
+	int running;
+	pthread_t thread;
+};
+
+static int STDCALL aio_poll_worker(void* param);
+
+static void aio_poll_init_idles(struct aio_poll_t* poll)
+{
+	int i;
+	struct aio_poll_socket_t* s;
+	for (i = 0; i < N_SOCKETS; i++)
+	{
+		s = (struct aio_poll_socket_t*)(poll + 1) + i;
+		list_insert_after(&s->link, poll->root.next);
+	}
+}
+
+static struct aio_poll_socket_t* aio_poll_alloc(struct aio_poll_t* poll)
+{
+	struct list_head* item;
+	locker_lock(&poll->locker);
+	if (list_empty(&poll->idles))
+	{
+		locker_unlock(&poll->locker);
+		return NULL;
+	}
+
+	item = poll->idles.next;
+	list_remove(item);
+	locker_unlock(&poll->locker);
+	return list_entry(item, struct aio_poll_socket_t, link);
+}
+
+static void aio_poll_free(struct aio_poll_t* poll, struct aio_poll_socket_t* s)
+{
+	// TODO: assert(idle count < N);
+	locker_lock(&poll->locker);
+	list_remove(&s->link);
+	list_insert_after(&s->link, poll->idles.prev);
+	locker_unlock(&poll->locker);
+}
+
+struct aio_poll_t* aio_poll_create()
+{
+	struct aio_poll_t* poll;
+	poll = (struct aio_poll_t*)calloc(1, sizeof(struct aio_poll_t) + sizeof(struct aio_poll_socket_t) * N_SOCKETS);
+	if (poll)
+	{
+		LIST_INIT_HEAD(&poll->root);
+		LIST_INIT_HEAD(&poll->idles);
+
+		poll->running = 1;
+		locker_create(&poll->locker);
+		thread_create(&poll->thread, aio_poll_worker, poll);
+	}
+	return poll;
+}
+
+int aio_poll_destroy(struct aio_poll_t* poll)
+{
+	if (!poll || 0 == poll->running)
+		return -1;
+
+	assert(list_empty(&poll->root));
+	poll->running = 0;
+	thread_destroy(poll->thread);
+	locker_destroy(&poll->locker);
+	return 0;
+}
+
+int aio_poll_poll(struct aio_poll_t* poll, socket_t socket, int flags, int timeout, aio_poll_callback callback, void* param)
+{
+	struct aio_poll_socket_t* s;
+
+	if (0 == (flags & (AIO_POLL_IN | AIO_POLL_OUT)) || !callback || timeout <= 0 || socket_invalid == socket)
+		return EINVAL;
+
+	s = aio_poll_alloc(poll);
+	if (!s)
+		return ENOMEM;
+
+	memset(s, 0, sizeof(*s));
+	s->fd = socket;
+	s->events = flags;
+	s->expire = system_clock() + timeout;
+	s->callback = callback;
+	s->param = param;
+
+	locker_lock(&poll->locker);
+	list_insert_after(&s->link, poll->root.prev);
+	locker_unlock(&poll->locker);
+	return 0;
+}
+
+static int aio_poll_do(struct aio_poll_socket_t* s[], int n, int timeout);
+static int STDCALL aio_poll_worker(void* param)
+{
+	int i, n, r;
+	uint64_t now;
+	struct list_head* ptr;
+	struct aio_poll_t* poll;
+	struct aio_poll_socket_t* links[N_SOCKETS];
+
+	poll = (struct aio_poll_t*)param;
+	while (poll->running)
+	{
+		n = 0;
+		locker_lock(&poll->locker);
+		list_for_each(ptr, &poll->root)
+		{
+			links[n++] = list_entry(ptr, struct aio_poll_socket_t, link);
+		}
+		locker_unlock(&poll->locker);
+
+		// TODO: timeout
+		r = aio_poll_do(links, n, 1000);
+		if (r < 0)
+			break;
+
+		now = system_clock();
+		for (i = 0; i < n; i++)
+		{
+			if (0 != links[i]->revents)
+			{
+				links[i]->callback(0, links[i]->fd, links[i]->revents, links[i]->param);
+				aio_poll_free(poll, links[i]);
+			}
+			else if ((int64_t)(links[i]->expire - now) > 0)
+			{
+				links[i]->callback(ETIMEDOUT, links[i]->fd, 0, links[i]->param);
+				aio_poll_free(poll, links[i]);
+			}
+			else
+			{
+				// next loop
+			}
+		}
+
+	}
+
+	return r;
+}
+
+static int aio_poll_do(struct aio_poll_socket_t* s[], int n, int timeout)
+{
+	int i;
+	int r;
+
+#if defined(OS_WINDOWS)
+	fd_set rfds;
+	fd_set wfds;
+	fd_set efds;
+	struct timeval tv;
+
+	assert(n <= FD_SETSIZE);
+	FD_ZERO(&rfds);
+	FD_ZERO(&wfds);
+	FD_ZERO(&efds);
+	for (i = 0; i < n; i++)
+	{
+		if (s[i]->events & AIO_POLL_IN)
+			FD_SET(s[i]->fd, &rfds);
+		if (s[i]->events & AIO_POLL_OUT)
+			FD_SET(s[i]->fd, &wfds);
+	}
+
+	tv.tv_sec = timeout / 1000;
+	tv.tv_usec = (timeout % 1000) * 1000;
+	r = select(n, &rfds, &wfds, &efds, timeout < 0 ? NULL : &tv);
+	if (r <= 0)
+		return r;
+
+	for (r = i = 0; i < n && i < 64; i++)
+	{
+		s[i]->revents = 0;
+		if (FD_ISSET(s[i]->fd, &rfds))
+			s[i]->revents |= AIO_POLL_IN;
+		if (FD_ISSET(s[i]->fd, &wfds))
+			s[i]->revents |= AIO_POLL_OUT;
+	}
+
+	return r;
+#else
+	int j;
+	struct pollfd fds[N_SOCKETS];
+	assert(n < N_SOCKETS);
+	for (i = 0; i < n; i++)
+	{
+		fds[i].fd = s[i]->fd;
+		fds[i].revents = 0;
+		if (s[i]->events & AIO_POLL_IN)
+			fds[i].events = POLLIN;
+		if (s[i]->events & AIO_POLL_OUT)
+			fds[i].events = POLLOUT;
+	}
+
+	r = poll(fds, n, timeout);
+	while (-1 == r && EINTR == errno)
+		r = poll(fds, n, timeout);
+
+	for (r = i = 0; i < n && i < 64; i++)
+	{
+		s[i]->oflags = 0;
+		if (fds[i].revents & POLLIN)
+			s[i]->oflags |= AIO_POLL_IN;
+		if (fds[i].revents & POLLOUT)
+			s[i]->oflags |= AIO_POLL_OUT;
+	}
+
+	return r;
+#endif
+}
