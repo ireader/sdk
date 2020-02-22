564c92ce9c5c209852a81ba59defbbfcf7265412
tao3 <tao3@outlook.com>
2020-02-22 10:15:30 +0800

    channel_push_timeout/channel_pop_timeout

--- include/channel.h
+++ include/channel.h
@@ -8,14 +8,19 @@ extern "C" {
 struct channel_t;
 
 struct channel_t* channel_create(int capacity, int elementsize);
-void channel_destroy(struct channel_t** q);
+void channel_destroy(struct channel_t** pc);
 
-//void channel_clear(struct channel_t* q);
-int channel_count(struct channel_t* q);
+//void channel_clear(struct channel_t* c);
+int channel_count(struct channel_t* c);
 
 // block push/pop
-int channel_push(struct channel_t* q, const void* e);
-int channel_pop(struct channel_t* q, void* e);
+int channel_push(struct channel_t* c, const void* e);
+int channel_pop(struct channel_t* c, void* e);
+
+/// @param[in] timeout MS
+/// @return 0-success, WAIT_TIMEOUT-timeout, other-error
+int channel_push_timeout(struct channel_t* c, const void* e, int timeout);
+int channel_pop_timeout(struct channel_t* c, void* e, int timeout);
 
 #if defined(__cplusplus)
 }
--- source/channel.c
+++ source/channel.c
@@ -1,5 +1,6 @@
 #include "channel.h"
 #include "sys/sema.h"
+#include "sys/atomic.h"
 #include "sys/locker.h"
 #include <stdint.h>
 #include <stdlib.h>
@@ -15,8 +16,6 @@ struct channel_t
 	locker_t locker;
     sema_t reader;
     sema_t writer;
-    int nreaders;
-    int nwriters;
 };
 
 struct channel_t* channel_create(int capacity, int elementsize)
@@ -29,7 +28,7 @@ struct channel_t* channel_create(int capacity, int elementsize)
 		memset(c, 0, sizeof(*c));
 		locker_create(&c->locker);
 		sema_create(&c->reader, NULL, 0);
-		sema_create(&c->writer, NULL, 0);
+		sema_create(&c->writer, NULL, capacity);
 		c->elesize = elementsize;
 		c->capacity = capacity;
 		c->ptr = (uint8_t*)(c + 1);
@@ -37,17 +36,16 @@ struct channel_t* channel_create(int capacity, int elementsize)
 	return c;
 }
 
-void channel_destroy(struct channel_t** pq)
+void channel_destroy(struct channel_t** pc)
 {
 	struct channel_t* c;
 
-	if (!pq || !*pq)
+	if (!pc || !*pc)
 		return;
 
-	c = *pq;
-	*pq = NULL;
+	c = *pc;
+	*pc = NULL;
 
-	assert(0 == c->nreaders && 0 == c->nwriters);
 	sema_destroy(&c->reader);
 	sema_destroy(&c->writer);
 	locker_destroy(&c->locker);
@@ -68,61 +66,66 @@ int channel_count(struct channel_t* c)
 	return c->count;
 }
 
-static int channel_wait(struct channel_t* c, sema_t *sema, int* count)
-{
-	int r;
-
-    assert(*count >= 0);
-	*count += 1;
-    locker_unlock(&c->locker);
-
-	r = sema_wait(sema);
-    assert(0 == r);
-    
-	locker_lock(&c->locker);
-    return r;
-}
-
-static int channel_wakeup(struct channel_t* c, sema_t *sema, int* count)
-{
-    int r;
-    assert(*count >= 0);
-	if (*count < 1)
-		return 0;
-
-    *count -= 1;
-    r = sema_post(sema);
-    assert(0 == r);
-    return r;
-}
-
 int channel_push(struct channel_t* c, const void* e)
 {
+	sema_wait(&c->writer);
+	
 	locker_lock(&c->locker);
-    
-    assert(c->nwriters >= 0 && c->count <= c->capacity);
-	while (c->count >= c->capacity)
-        channel_wait(c, &c->writer, &c->nwriters);
-
+	assert(c->count < c->capacity);
 	memcpy(c->ptr + ((c->offset + c->count) % c->capacity) * c->elesize, e, c->elesize);
 	c->count += 1;
-	channel_wakeup(c, &c->reader, &c->nreaders);
 	locker_unlock(&c->locker);
+
+	sema_post(&c->reader);
 	return 0;
 }
 
 int channel_pop(struct channel_t* c, void* e)
 {
-	locker_lock(&c->locker);
-    
-    assert(c->nreaders >= 0 && c->count >= 0);
-	while (0 == c->count)
-        channel_wait(c, &c->reader, &c->nreaders);
+	sema_wait(&c->reader);
 
+	locker_lock(&c->locker);
 	memcpy(e, c->ptr + c->offset * c->elesize, c->elesize);
 	c->count -= 1;
 	c->offset = (c->offset + 1) % c->capacity;
-	channel_wakeup(c, &c->writer, &c->nwriters);
+	assert(c->count >= 0);
 	locker_unlock(&c->locker);
+
+	sema_post(&c->writer);
+	return 0;
+}
+
+int channel_push_timeout(struct channel_t* c, const void* e, int timeout)
+{
+	int r;
+	r = sema_timewait(&c->writer, timeout);
+	if (0 != r)
+		return r;
+
+	locker_lock(&c->locker);
+	assert(c->count < c->capacity);
+	memcpy(c->ptr + ((c->offset + c->count) % c->capacity) * c->elesize, e, c->elesize);
+	c->count += 1;
+	locker_unlock(&c->locker);
+
+	sema_post(&c->reader);
+	return 0;
+}
+
+int channel_pop_timeout(struct channel_t* c, void* e, int timeout)
+{
+	int r;
+	r = sema_timewait(&c->reader, timeout);
+	if (0 != r)
+		return r;
+
+	locker_lock(&c->locker);
+	memcpy(e, c->ptr + c->offset * c->elesize, c->elesize);
+	c->count -= 1;
+	c->offset = (c->offset + 1) % c->capacity;
+	assert(c->count >= 0);
+	locker_unlock(&c->locker);
+
+	sema_post(&c->writer);
 	return 0;
 }
