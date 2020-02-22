f06d1eb0ed9b24a27ab5ab748ca737dc0b22f3d0
tao3 <tao3@outlook.com>
2020-02-22 10:14:43 +0800

    add BOUND/FLOOR/CEIL

--- include/cpm/param.h
+++ include/cpm/param.h
@@ -1,30 +1,105 @@
 #ifndef _cpm_param_h_
 #define _cpm_param_h_
 
-#ifndef __cplusplus
-#ifndef min
+// MIN
+// MAX
+// BOUND(10, 1, 100) ==> 10
+// FLOOR(34, 10) ==> 3
+// CEIL(34, 10) ==> 4
+
+#if defined(__cplusplus)
+#include <algorithm>
+
+#ifndef MIN
+#undef min // msvc stdlib.h
+#define MIN(x, y) std::min(x, y)
+#endif
+
+#ifndef MAX
+#undef max // msvc stdlib.h
+#define MAX(x, y) std::max(x, y)
+#endif
+
+#ifndef FLOOR
+#define FLOOR(num, deno) __FLOOR(num, deno)
+template <class T>
+inline T __FLOOR(const T& num, const T& demo)
+{
+	return num / demo;
+}
+#endif
+
+#ifndef CEIL
+#define CEIL(num, deno) __CEIL(num, deno)
+template <class T>
+inline T __CEIL(const T& num, const T& demo)
+{
+	return (num + demo - 1) / demo;
+}
+#endif
+
+#else // __cplusplus
+
 #if __GNUC__
-#define min(x, y) ( __extension__ ({ \
+#ifndef MIN
+#define MIN(x, y) ( __extension__ ({ \
 			typeof(x) _min1 = (x);          \
 			typeof(y) _min2 = (y);          \
 			(void) (&_min1 == &_min2);      \
 			_min1 < _min2 ? _min1 : _min2; }))
-#else
-#define min(x, y) ((x) < (y) ? (x) : (y))
 #endif
-#endif // min
 
-#ifndef max
-#if __GNUC__
-#define max(x, y) ( __extension__ ({ \
+#ifndef MAX
+#define MAX(x, y) ( __extension__ ({ \
 			typeof(x) _max1 = (x);          \
 			typeof(y) _max2 = (y);          \
 			(void) (&_max1 == &_max2);      \
 			_max1 > _max2 ? _max1 : _max2; }))
-#else
-#define max(x, y) ((x) > (y) ? (x) : (y))
 #endif
-#endif // max
+
+#ifndef FLOOR
+#define FLOOR(num, deno) ( __extension__ ({ \
+			typeof(num) _num1 = (num);		\
+			typeof(deno) _deno1 = (deno);	\
+			(void) (&_num1 == &_deno1);		\
+			_num1 / _deno1; }))
+#endif
+
+#ifndef CEIL
+#define CEIL(num, deno) ( __extension__ ({ \
+			typeof(num) _num1 = (num);		\
+			typeof(deno) _deno1 = (deno);	\
+			(void) (&_num1 == &_deno1);		\
+			(_num1 + _deno1 - 1) / _deno1; }))
+#endif
+
+#else // __GNUC__
+
+#ifndef MIN
+#define MIN(x, y) ((x) < (y) ? (x) : (y))
+#endif
+
+#ifndef MAX
+#define MAX(x, y) ((x) > (y) ? (x) : (y))
+#endif
+
+// FLOOR(num/demo): FLOOR(34/10) = 3 
+#ifndef FLOOR
+#define FLOOR(num, deno) ( (num)/ (deno) )
+#endif
+
+// CEIL(num/demo): CEIL(34/10) = CEIL((34 + 10 - 1) / 10) = 4
+#ifndef CEIL
+#define CEIL(num, deno) ( ((num) + (deno) - 1) / (deno) )
+#endif
+
+#endif // __GNUC__
 #endif // __cplusplus
+
+
+// BOUND(value, 2, 20) => [2 ~ 20]
+#ifndef BOUND
+#define BOUND(v, min, max) MIN(MAX(v, min), max)
+#endif
 
 #endif /* !_cpm_param_h_ */
