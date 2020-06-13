91070a25b0cab4523df3dbad86dd2933c298084c
tao3 <tao3@outlook.com>
2020-06-13 11:16:57 +0800

    add path_resolve

--- include/sys/path.h
+++ include/sys/path.h
@@ -17,6 +17,7 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdarg.h>
 #include <sys/stat.h>
 
 /// test file
@@ -189,6 +190,100 @@ static inline int64_t path_filesize(const char* filename)
 		return st.st_size;
 	return -1;
 #endif
+}
+
+static inline int path_isabsolute(const char* path, size_t n)
+{
+    size_t off;
+    if (n < 1)
+        return 0;
+
+    if ('/' == path[0])
+        return 1;
+
+#if defined(OS_WINDOWS)
+    // 1. \ (relative to current working directory root)
+    // 2. [driver_letter]:\
+    // 3. \\[server]\]sharename]\
+    // 4. \\?\[driver_spec]:\
+    // 5. \\?\[server]\[sharename]\
+    // 6. \\?\UNC\[server]\[sharename]\
+    // 7. \\.\[physical_device]\  e.g. \\.\COM1\ 
+
+    if (n > 3)
+    {
+        if (':' == path[1] && '\\' == path[2] && (('a' <= path[0] && 'z' >= path[0]) || ('A' <= path[0] && 'Z' >= path[0])))
+            return 1; // case 2
+        else if ('\\' == path[0] && '\\' == path[1] && '\\' != path[3])
+            return 1; // case 3/4/5/6/7
+        else if ('\\' == path[0] && '\\' != path[1])
+            return 1; // case 1
+    }
+#endif
+
+#if 1
+    // url schema
+    off = strcspn(path, ":/\\");
+    if (off + 2 < n && ':' == path[off] && '/' == path[off + 1] && '/' == path[off + 2])
+        return 1; // e.g. http://
+#endif
+
+    return 0;
+}
+
+/// path_resolve("abc.txt", "/path/to/dir") ==> /path/to/dir/abc.txt
+/// @return 0-ok with non-absolute directory, 1-ok with absolute directory, <0-error
+static inline int path_resolve(char* buf, size_t len, const char* path, const char* base)
+{
+    int r;
+    size_t n, m;
+
+    n = path ? strlen(path) : 0;
+    m = base ? strlen(base) : 0;
+    r = path_isabsolute(path, n);
+    if (m > 0 && 0 == r)
+    {
+        r = path_isabsolute(base, m);
+        while (m > 0 && ('/' == base[m - 1] || '\\' == base[m - 1]))
+            m--; // filter last /
+
+        if (m + n + 1 /* '/' */ + 1 /* '\0' */ > len)
+            return -1;
+
+        memmove(buf + m + 1, path, n + 1); // with '\0'
+        memmove(buf, base, m);
+        buf[m] = '/'; // TODO: windows \ separator
+        return r;
+    }
+    else
+    {
+        if (n + 1 > len)
+            return -1;
+
+        //path_resolve2 memory safe: snprintf(buf, len, "%s", path);
+        memmove(buf, path, n + 1); // with '\0'
+        return r;
+    }
+}
+
+/// path_resolve2("abc.txt", "path/to/dir", "/root/") ==> /root/path/to/dir/abc.txt
+/// path_resolve2("abc.txt", "/root1", "/root2/") ==> /root1/abc.txt
+/// @return 0-ok with non-absolute directory, 1-ok with absolute directory, <0-error
+static inline int path_resolve2(char* buf, size_t len, const char* path, const char* base1, ...)
+{
+    int r;
+    va_list va;
+    r = path_resolve(buf, len, path, base1);
+
+    va_start(va, base1);
+    while (0 == r)
+    {
+        base1 = va_arg(va, const char*);
+        r = path_resolve(buf, len, buf, base1); // memory safe with buf
+    }
+    va_end(va);
+
+    return r;
 }
 
 #endif /* !_platform_path_h_ */
