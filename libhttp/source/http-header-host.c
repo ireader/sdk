44be563f133ca2752353f98b0138365c536a6aba
tao3 <tao3@outlook.com>
2020-06-14 01:53:07 +0800

    ipv6 host

--- libhttp/source/http-header-host.c
+++ libhttp/source/http-header-host.c
@@ -8,25 +8,48 @@ int http_header_host(const char* field, char host[], size_t bytes, unsigned shor
 	size_t n;
 	const char* p;
 
-	p = strchr(field, ':');
-	if(p)
+	if (!field || !host || bytes < 1)
+		return -1;
+
+	if ('[' == field[0])
 	{
-        n = (size_t)(p - field); // ptrdiff_t -> size_t
-		if(bytes <= n)
+		field++; //skip '['
+		p = strchr(field, ']');
+		if (!p)
 			return -1;
 
-		memcpy(host, field, n);
-		host[n] = '\0';
-		*port = (unsigned short)atoi(p+1);
+		n = (size_t)(p - field); // ptrdiff_t -> size_t
+		p++;
 	}
 	else
 	{
-		n = strlen(field);
-		if(bytes <= n)
-			return -1;
+		p = strchr(field, ':');
+		if (p)
+		{
+			// if more than one ':', assume to IPv6
+			n = strchr(p+1, ':') ? strlen(field) : (size_t)(p - field); // ptrdiff_t -> size_t
+			if (bytes <= n)
+				return -1;
+			p = field + n; // skip IPv6 port
+		}
+		else
+		{
+			n = strlen(field);
+		}
+	}
 
-		memcpy(host, field, n);
-		host[n] = '\0';
+	if (bytes <= n)
+		return -1;
+
+	// copy host
+	memcpy(host, field, n);
+	host[n] = '\0';
+
+	if (p && ':' == *p)
+	{
+		*port = (unsigned short)strtol(p + 1, &p, 10);
+		if (0 != *p)
+			return -1; // should be end with port
 	}
 
 	return 0;
@@ -44,7 +67,9 @@ void http_header_host_test(void)
 	port = 0;
 	assert(0 == http_header_host("114.21.2.11", host, sizeof(host), &port) && 0==port && 0==strcmp("114.21.2.11", host));
 	assert(0 == http_header_host("114.21.2.11:8081", host, sizeof(host), &port) && 8081==port && 0==strcmp("114.21.2.11", host));
-
+	assert(0 == http_header_host("[2001:db8::9:1]:8081", host, sizeof(host), &port) && 8081 == port && 0 == strcmp("2001:db8::9:1", host));
+	assert(0 == http_header_host("2001:db8::9:1", host, sizeof(host), &port) && 0 == strcmp("2001:db8::9:1", host));
+	
 	assert(0 != http_header_host("www.verylongdnsname:com:80", host, sizeof(host), &port));
 }
 #endif
