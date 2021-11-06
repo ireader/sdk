5121df892a00168cfdadde28ba8fa00a4485f6b9
ireader <tao3@outlook.com>
2021-11-06 08:26:10 +0800

    set win32 minidump flags to MIDIDUMP

--- source/port/win32-minidump.c
+++ source/port/win32-minidump.c
@@ -3,20 +3,27 @@
 
 #pragma comment(lib, "Dbghelp.lib")
 
+#define TINYDUMP MiniDumpNormal
+#define MINIDUMP (MiniDumpWithIndirectlyReferencedMemory | MiniDumpScanMemory)
+#define MIDIDUMP (MiniDumpWithPrivateReadWriteMemory | MiniDumpWithDataSegs | MiniDumpWithHandleData | MiniDumpWithFullMemoryInfo | MiniDumpWithThreadInfo | MiniDumpWithUnloadedModules)
+#define MAXIDUMP (MiniDumpWithFullMemory | MiniDumpWithFullMemoryInfo | MiniDumpWithHandleData | MiniDumpWithThreadInfo | MiniDumpWithUnloadedModules)
+
 LONG WINAPI CreateMiniDump(const char* filename, struct _EXCEPTION_POINTERS* pep)
 {
     HANDLE hFile;
+    MINIDUMP_TYPE flags;
     MINIDUMP_EXCEPTION_INFORMATION info;
     
     hFile = CreateFileA(filename, GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
     if (hFile == INVALID_HANDLE_VALUE)
         return EXCEPTION_CONTINUE_SEARCH;
 
+    flags = MIDIDUMP;
     memset(&info, 0, sizeof(info));
     info.ThreadId = GetCurrentThreadId();
     info.ExceptionPointers = pep;
     info.ClientPointers = FALSE;
-    MiniDumpWriteDump(GetCurrentProcess(), GetCurrentProcessId(), hFile, MiniDumpNormal|MiniDumpWithThreadInfo, &info, 0, 0);
+    MiniDumpWriteDump(GetCurrentProcess(), GetCurrentProcessId(), hFile, flags, &info, 0, 0);
 
     CloseHandle(hFile);
     return EXCEPTION_EXECUTE_HANDLER;
