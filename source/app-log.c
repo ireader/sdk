98b0edad94542be48ac855f5c047c95983545894
ireader <tao3@outlook.com>
2022-06-11 11:27:40 +0800

    change: warning log color

--- source/app-log.c
+++ source/app-log.c
@@ -25,7 +25,7 @@
 //static const char s_month[][4] = { "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" };
 //static const char* s_level_tag[] = { "EMERG", "ALERT", "CRIT", "ERROR", "WARNING", "NOTICE", "INFO", "DEBUG" };
 static const char* s_level_tag[] = { "X", "A", "C", "E", "W", "N", "I", "D" };
-static const char* s_level_color_default[] = { "\033[0m", "\033[0;35m", "\033[0;34m", "\033[0;33m", "\033[0;31m", "\033[0;31m", "\033[0;37m", "\033[0;32m", "\033[0m", };
+static const char* s_level_color_default[] = { "\033[0m", "\033[0;35m", "\033[0;34m", "\033[0;33m", "\033[0;33m", "\033[0;31m", "\033[0;37m", "\033[0;32m", "\033[0m", };
 static const char* s_level_color_none[] = { "", "", "", "", "", "", "", "", "", };
 static const char** s_level_color = s_level_color_none;
 
