2cfc8c707e1d89551b640a5cc9a2548132d3a273
ireader <tao3@outlook.com>
2022-09-24 15:39:12 +0800

    change declare with extern "C"

--- libhttp/include/rfc822-datetime.h
+++ libhttp/include/rfc822-datetime.h
@@ -3,8 +3,15 @@
 
 #include <time.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 typedef char rfc822_datetime_t[30];
 
 const char* rfc822_datetime_format(time_t time, rfc822_datetime_t datetime);
 
+#ifdef __cplusplus
+}
+#endif
 #endif /* !_rfc822_datetime_h_ */
